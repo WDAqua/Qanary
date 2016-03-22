5e157d2e63671b636121223898b61fd5c2e735f5
Andreas Both <andreas.both.de@gmail.com>
2016-03-22 01:36:17 +0100

    template Qanary implementation (running as dummy component out-of-the-box)

---
+++ src/main/java/eu/wdaqua/qanary/component/QanaryComponent.java
@@ -0,0 +1,21 @@
+package eu.wdaqua.qanary.component;
+
+import eu.wdaqua.qanary.QanaryMessage;
+
+/**
+ * represent the behavior of an annotator following the Qanary methodology
+ * 
+ * @author AnBo
+ *
+ */
+public interface QanaryComponent {
+
+	/**
+	 * needs to be implemented for any new Qanary component
+	 * 
+	 * @param myQanaryMessage
+	 * @return
+	 */
+	public QanaryMessage process(QanaryMessage myQanaryMessage);
+
+}
---
+++ src/main/java/eu/wdaqua/qanary/component/QanaryComponentImpl.java
@@ -0,0 +1,24 @@
+package eu.wdaqua.qanary.component;
+
+import org.springframework.stereotype.Component;
+
+import eu.wdaqua.qanary.QanaryMessage;
+
+/**
+ * represents a trivial Qanary component, not doing anything (can be removed at
+ * the end)
+ * 
+ * @author AnBo
+ *
+ */
+@Component
+public class QanaryComponentImpl implements QanaryComponent {
+
+	/**
+	 * default processor of a QanaryMessage
+	 */
+	public QanaryMessage process(QanaryMessage myQanaryMessage) {
+		return myQanaryMessage;
+	}
+
+}
