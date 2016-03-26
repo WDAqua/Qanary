acf84773d7d78180d434b99c5d4d836bd95751df
Andreas Both <andreas.both@mercateo.com>
2016-03-26 15:10:22 +0100

    integrated Spring Boot Admin server, c.f. https://github.com/codecentric/spring-boot-admin - pipeline itself is client of admin server, might be removed in the future

--- qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/QanaryPipeline.java
+++ qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/QanaryPipeline.java
@@ -2,12 +2,18 @@ package eu.wdaqua.qanary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient // registers itself as client for the admin server,
+						// removeable
 public class QanaryPipeline {
 
-    public static void main(String[] args) {
-        SpringApplication.run(QanaryPipeline.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(QanaryPipeline.class, args);
+	}
 
 }
