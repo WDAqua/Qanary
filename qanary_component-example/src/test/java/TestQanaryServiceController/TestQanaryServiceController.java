bfe3f90b545a24e79d26eb27074853d6a492c198
Andreas Both <andreas.both.de@gmail.com>
2016-03-26 01:36:49 +0100

    renamed all projects to qa.*

--- qanary_component-example/src/test/java/TestQanaryServiceController/TestQanaryServiceController.java
+++ qanary_component-example/src/test/java/TestQanaryServiceController/TestQanaryServiceController.java
@@ -1,7 +1,8 @@
-package eu.wdaqua.qanary.examplecomponent;
+package TestQanaryServiceController;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,18 +15,32 @@ import javax.inject.Inject;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Assert;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import eu.wdaqua.qanary.QanaryMessage;
+import eu.wdaqua.qanary.component.QanaryService;
 import eu.wdaqua.qanary.component.QanaryServiceController;
 import eu.wdaqua.qanary.config.QanaryConfiguration;
 import net.minidev.json.JSONObject;
 
-public class ExampleQanaryComponentTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { QanaryService.class }, loader = SpringApplicationContextLoader.class)
+@WebAppConfiguration
+public class TestQanaryServiceController {
+
+	private static final Logger logger = LoggerFactory.getLogger(TestQanaryServiceController.class);
 
 	@Inject
 	QanaryServiceController controller;
@@ -47,6 +62,24 @@ public class ExampleQanaryComponentTest {
 	}
 
 	/**
+	 * test description interface
+	 */
+	@Test
+	public void testDescriptionAvailable() {
+
+		MvcResult result;
+		try {
+			result = mockMvc.perform(get(QanaryConfiguration.description)) // fetch
+					.andExpect(status().isOk()) // HTTP 200
+					.andExpect(content().contentType(MediaType.TEXT_HTML)) //
+					.andReturn(); // HTML
+			assert (result.getRequest().getContentLength() > 0);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	/**
 	 * send and receive message a JSON message to
 	 * QanaryConfiguration.annotatequestion, check if the values are the same
 	 */
@@ -60,8 +93,8 @@ public class ExampleQanaryComponentTest {
 		JSONObject jsonObject = new JSONObject();
 		// TODO: replace key by URLs of the qa vocabulary
 		jsonObject.put(QanaryMessage.endpointKey, testEndPoint);
-		jsonObject.put(QanaryMessage.inGraph, testInGraph);
-		jsonObject.put(QanaryMessage.outGraph, testOutGraph);
+		jsonObject.put("ingraph", testInGraph);
+		jsonObject.put("outgraph", testOutGraph);
 
 		// create message from json string
 		QanaryMessage requestMessage;
@@ -80,7 +113,7 @@ public class ExampleQanaryComponentTest {
 							post(QanaryConfiguration.annotatequestion) //
 									.content(requestMessage.asJsonString()) //
 									.contentType(MediaType.APPLICATION_JSON))
-					.andExpect(status().is2xxSuccessful()) //
+					.andExpect(status().isOk()) // ok
 					.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)) //
 					.andReturn();
 		} catch (Exception e) {
@@ -104,6 +137,50 @@ public class ExampleQanaryComponentTest {
 			assertTrue("check result vs. request: " + key, compareResult == 0);
 		}
 
+	}
+
+	/**
+	 * test correct message format
+	 */
+	@Test
+	public void testMessageFromJson() {
+
+		String testEndPoint = "http://qanary.test/endpoint";
+		String testInGraph = "http://qanary.test/graph/in";
+		String testOutGraph = "http://qanary.test/graph/out";
+
+		// create a JSON object with required properties
+		JSONObject jsonObject = new JSONObject();
+		// TODO: replace key by URLs of the qa vocabulary
+		jsonObject.put(QanaryMessage.endpointKey, testEndPoint);
+		jsonObject.put("ingraph", testInGraph);
+		jsonObject.put("outgraph", testOutGraph);
+
+		// create message from json string
+		QanaryMessage message;
+		try {
+			message = new QanaryMessage(jsonObject.toJSONString());
+
+			URL endpointKeyUrlFromMessage = message.get(new URL(QanaryMessage.endpointKey));
+			Assert.notNull(endpointKeyUrlFromMessage);
+
+			URL endpointKeyUrlFromHere = new URL(testEndPoint);
+
+			// TODO: more tests to ensure mechanism
+			assertTrue(endpointKeyUrlFromHere.toString().compareTo(endpointKeyUrlFromMessage.toString()) == 0);
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+	}
+
+	@Test
+	public void testAnnotation() {
+
+		// QanaryMessage message = new QanaryMessage()
+		// mockMvc.perform
 	}
 
 }
