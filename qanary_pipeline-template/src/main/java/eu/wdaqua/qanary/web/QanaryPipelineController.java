f099ef69c74ae76ac865d6f43d7c2f3f7939f15b
Andreas Both <andreas.both.de@gmail.com>
2016-04-06 01:42:10 +0200

    complete life cycle of question creation following RESTful principles - get all questions GET /question - create question POST /question - delete question DELETE /question/{questionID} - get information to a specific question GET /question/{questionID} - get raw data of GET /question/{questionID}/raw - for convenience a web ui is provided for creating tetual questions GET /inputtextquestion

--- qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/web/QanaryPipelineController.java
+++ qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/web/QanaryPipelineController.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -18,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +27,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import eu.wdaqua.qanary.business.QanaryConfigurator;
+import eu.wdaqua.qanary.message.QanaryAvailableQuestions;
 import eu.wdaqua.qanary.message.QanaryQuestionCreated;
+import eu.wdaqua.qanary.message.QanaryQuestionInformation;
 
 @Controller
 public class QanaryPipelineController {
@@ -33,9 +37,19 @@ public class QanaryPipelineController {
 	private static final Logger logger = LoggerFactory.getLogger(QanaryPipelineController.class);
 
 	// TODO: define directory in config
-	String directoryForSotringQuestionRawData = "/tmp";
+	String directoryForSotringQuestionRawData = "/tmp/questions";
 
 	private QanaryConfigurator qanaryConfigurator;
+
+	/**
+	 * return directory where the raw questions are stored or saved if not
+	 * existing, then create the directory
+	 * 
+	 * @return
+	 */
+	String getDirectoryForStoringQuestionRawData() {
+		return this.directoryForSotringQuestionRawData;
+	}
 
 	/**
 	 * get some information about your business using the fancy HTML template
@@ -81,7 +95,7 @@ public class QanaryPipelineController {
 
 		// store the received question in a file
 		String filename = UUID.randomUUID().toString();
-		String filepath = Paths.get(this.directoryForSotringQuestionRawData, "question_" + filename).toString();
+		String filepath = Paths.get(this.directoryForSotringQuestionRawData, filename).toString();
 
 		// Save the file locally
 		try {
@@ -101,7 +115,7 @@ public class QanaryPipelineController {
 
 		QanaryQuestionCreated responseMessage;
 		try {
-			URI uriOfQuestion = new URI("http://localhost:8080/question/" + filename);
+			URI uriOfQuestion = new URI(this.getHost() + "/question/" + filename);
 			logger.info("uriOfQuestion: " + uriOfQuestion);
 			responseMessage = new QanaryQuestionCreated(uriOfQuestion);
 		} catch (URISyntaxException e) {
@@ -114,16 +128,71 @@ public class QanaryPipelineController {
 	}
 
 	/**
+	 * synchronous call to start the QA process (POST), return the URL of the
+	 * created question
+	 * 
+	 * @param questionstring
+	 * @throws HttpRequestMethodNotSupportedException
+	 */
+	@RequestMapping(value = "/question/{questionid}", method = RequestMethod.DELETE, produces = "application/json")
+	@ResponseBody
+	public String deleteQuestion(@PathVariable String questionid) throws HttpRequestMethodNotSupportedException {
+		throw new HttpRequestMethodNotSupportedException("not yet implemented");
+	}
+
+	/**
+	 * returns the current name of the host
+	 * 
+	 * @return
+	 */
+	private String getHost() {
+		// @TODO: replace by configuration
+		return "http://localhost:8080";
+	}
+
+	/**
+	 * return links to all questions
+	 * 
+	 * @param questionstring
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/question/", method = RequestMethod.GET)
+	@ResponseBody
+	public QanaryAvailableQuestions getQuestions() throws IOException {
+
+		QanaryAvailableQuestions questions = new QanaryAvailableQuestions(this.getDirectoryForStoringQuestionRawData(),
+				this.getHost());
+
+		logger.debug("Number of available questions in " + this.getDirectoryForStoringQuestionRawData() + ": "
+				+ questions.getAvailableQuestions().size());
+
+		return questions;
+	}
+
+	/**
+	 * return links to all information of one given question
+	 * 
+	 * @param questionstring
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/question/{questionid}", method = RequestMethod.GET)
+	@ResponseBody
+	public QanaryQuestionInformation getQuestion(@PathVariable String questionid) throws MalformedURLException {
+		return new QanaryQuestionInformation(questionid, this.getHost());
+	}
+
+	/**
 	 * question for a given id, raw return of the data
 	 * 
 	 * @param questionstring
 	 */
-	@RequestMapping(value = "/question/{questionid}", method = RequestMethod.GET, produces = MediaType.ALL_VALUE)
+	@RequestMapping(value = "/question/{questionid}/raw", method = RequestMethod.GET, produces = MediaType.ALL_VALUE)
 	@ResponseBody
-	public String getQuestion(@PathVariable String questionid) {
+	public String getQuestionRawData(@PathVariable String questionid) {
 
 		// TODO: move outside of this class
-		String filename = Paths.get(this.directoryForSotringQuestionRawData, "question_" + questionid).toString();
+		String filename = Paths.get(this.getDirectoryForStoringQuestionRawData(), questionid).toString();
 		String content = null;
 		File file = new File(filename);
 		FileReader reader = null;
