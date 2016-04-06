d998f42d09056e8f3e30dc9b950b1321137e04f2
Andreas Both <andreas.both@mercateo.com>
2016-04-06 10:29:24 +0200

    goal: make question and raw data of question available in component
    - changed QanaryComponent from interface to abstract class
    - current code will NOT work

--- src/main/java/eu/wdaqua/qanary/config/QanaryConfiguration.java
+++ src/main/java/eu/wdaqua/qanary/config/QanaryConfiguration.java
@@ -7,4 +7,5 @@ public class QanaryConfiguration {
 	public static final String description = "/description";
 	public static final String annotatequestion = "/annotatequestion";
 	public static final String sparql = "/sparql";
+	public static final String questionRawDataUrlSuffix = "/raw";
 }
