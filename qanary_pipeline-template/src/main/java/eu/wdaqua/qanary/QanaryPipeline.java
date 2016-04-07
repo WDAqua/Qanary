e67f9d5cb2c8c5a3d9d3f2c76de7771eefafe8eb
Andreas Both <andreas.both@mercateo.com>
2016-04-07 11:20:28 +0200

    improves service implementation for starting a question answering process (unfinished)
    - /startquestionanswering : see a simple Web UI for creating a QA process
    - /questionanswering : processing of question (URI)

--- qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/QanaryPipeline.java
+++ qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/QanaryPipeline.java
@@ -1,71 +1,74 @@
 package eu.wdaqua.qanary;
 
-import com.google.common.collect.Maps;
-import de.codecentric.boot.admin.event.ClientApplicationDeregisteredEvent;
-import de.codecentric.boot.admin.event.ClientApplicationEvent;
-import de.codecentric.boot.admin.event.ClientApplicationRegisteredEvent;
-import eu.wdaqua.qanary.business.QanaryComponent;
-import eu.wdaqua.qanary.business.QanaryConfigurator;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.constraints.NotNull;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Map;
+import com.google.common.collect.Maps;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.event.ClientApplicationDeregisteredEvent;
+import de.codecentric.boot.admin.event.ClientApplicationEvent;
+import de.codecentric.boot.admin.event.ClientApplicationRegisteredEvent;
+import eu.wdaqua.qanary.business.QanaryConfigurator;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient // registers itself as client for the admin server,
 // removeable
-@ComponentScan({"eu.wdaqua.qanary.business", "eu.wdaqua.qanary.web"})
+@ComponentScan({ "eu.wdaqua.qanary.business", "eu.wdaqua.qanary.web" })
 public class QanaryPipeline {
 
-    public static void main(String[] args) {
-        SpringApplication.run(QanaryPipeline.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(QanaryPipeline.class, args);
+	}
 
-    @Autowired
-    public QanaryConfigurator configurator;
+	@Autowired
+	public QanaryConfigurator configurator;
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
-    @Bean
-    public Map<String, Integer> componentsToIndexMap(@Value("'${qanary.components}'.split(',')") List<String> components) {
-        int i = 0;
-        Map<String, Integer> componentsToIndexMap = Maps.newHashMap();
-        for (String component : components) {
-            componentsToIndexMap.put(component, i);
-            i++;
-        }
-        return componentsToIndexMap;
-    }
+	@Bean
+	public Map<String, Integer> componentsToIndexMap(
+			@Value("'${qanary.components}'.split(',')") List<String> components) {
+		int i = 0;
+		Map<String, Integer> componentsToIndexMap = Maps.newHashMap();
+		for (String component : components) {
+			componentsToIndexMap.put(component, i);
+			i++;
+		}
+		return componentsToIndexMap;
+	}
 
-    @Bean
-    public QanaryConfigurator configurator(@Value("'${qanary.components}'.split(',')") List<String> components){
-        return new QanaryConfigurator(restTemplate(),componentsToIndexMap(components));
-    }
+	@Bean
+	public QanaryConfigurator configurator(@Value("'${qanary.components}'.split(',')") List<String> components,
+			@Value("${server.host}") @NotNull String host, @Value("${server.port}") @NotNull int port,
+			@Value("${qanary.triplestore}") @NotNull URL endpoint) {
+		return new QanaryConfigurator(restTemplate(), componentsToIndexMap(components), host, port, endpoint);
+	}
 
+	@EventListener(ClientApplicationRegisteredEvent.class)
+	public void addComponent(ClientApplicationEvent event) {
+		configurator.addComponent(event.getApplication());
+	}
 
-    @EventListener(ClientApplicationRegisteredEvent.class)
-    public void addComponent(ClientApplicationEvent event) {
-        configurator.addComponent(event.getApplication());
-    }
-
-    @EventListener(ClientApplicationDeregisteredEvent.class)
-    public void removeComponent(ClientApplicationEvent event) {
-        configurator.removeComponent(event.getApplication());
-    }
+	@EventListener(ClientApplicationDeregisteredEvent.class)
+	public void removeComponent(ClientApplicationEvent event) {
+		configurator.removeComponent(event.getApplication());
+	}
 }
--- qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/business/QanaryConfigurator.java
+++ qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/business/QanaryConfigurator.java
@@ -1,75 +1,87 @@
 package eu.wdaqua.qanary.business;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import de.codecentric.boot.admin.model.Application;
-import eu.wdaqua.qanary.message.QanaryMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import de.codecentric.boot.admin.model.Application;
+import eu.wdaqua.qanary.message.QanaryMessage;
 
 /**
  * Created by didier on 27.03.16.
  */
 public class QanaryConfigurator {
 
-    private final RestTemplate restTemplate;
-    private List<QanaryComponent> components;
-    private final Map<String, Integer> componentsToIndexMap;
-    private final String port="8080";
+	private final RestTemplate restTemplate;
+	private List<QanaryComponent> components;
+	private final Map<String, Integer> componentsToIndexMap;
+	private final int port;
+	private final String host;
+	private final URL endpoint;
 
-    public QanaryConfigurator(RestTemplate restTemplate, Map<String, Integer> componentsToIndexMap) {
-        this.restTemplate = restTemplate;
-        this.componentsToIndexMap = Maps.newHashMap();
-        this.components = Lists.newArrayList();
-    }
+	public QanaryConfigurator(RestTemplate restTemplate, Map<String, Integer> componentsToIndexMap, String host,
+			int port, URL endpoint) {
+		this.restTemplate = restTemplate;
+		this.componentsToIndexMap = Maps.newHashMap();
 
-    /**
-     * Call the url of the services
-     */
-    public void callServices(QanaryMessage message) {
-        List<QanaryComponent> componentsToUse = components.stream()
-                .filter(qanaryComponent -> qanaryComponent.isUsed())
-                .collect(Collectors.toList());
+		// TODO: from config
+		this.components = Lists.newArrayList();
 
-        for (QanaryComponent component : componentsToUse) {
-            HttpEntity<QanaryMessage> request = new HttpEntity<QanaryMessage>(message);
-            ResponseEntity<QanaryMessage> responseEntity = restTemplate.exchange(component.getUrl(), HttpMethod.POST,
-                    request, QanaryMessage.class);
-            if(responseEntity.getStatusCode() == HttpStatus.OK){
-                message = responseEntity.getBody();
-            }
-        }
-    }
+		// from config
+		this.port = port;
+		this.host = host;
+		this.endpoint = endpoint;
+	}
 
-    public void addComponent(Application application) {
-        if (componentsToIndexMap.containsKey(application.getName())) {
-            Integer index = Math.min(components.size(), componentsToIndexMap.get(application.getName()));
-            components.add(index, new QanaryComponent(application, true));
-        }
-        components.add(new QanaryComponent(application, false));
-    }
+	/**
+	 * Call the url of the services
+	 */
+	public void callServices(QanaryMessage message) {
+		List<QanaryComponent> componentsToUse = components.stream().filter(qanaryComponent -> qanaryComponent.isUsed())
+				.collect(Collectors.toList());
 
-    public void removeComponent(Application application) {
-        components.removeIf(
-                qanaryComponent -> qanaryComponent.getName().equals(application.getName()
-                )
-        );
-    }
-    
-    // TODO: Implement set by configuration
-    public String getPort(){
-    	return this.port;
-    }
+		for (QanaryComponent component : componentsToUse) {
+			HttpEntity<QanaryMessage> request = new HttpEntity<QanaryMessage>(message);
+			ResponseEntity<QanaryMessage> responseEntity = restTemplate.exchange(component.getUrl(), HttpMethod.POST,
+					request, QanaryMessage.class);
+			if (responseEntity.getStatusCode() == HttpStatus.OK) {
+				message = responseEntity.getBody();
+			}
+		}
+	}
+
+	public void addComponent(Application application) {
+		if (componentsToIndexMap.containsKey(application.getName())) {
+			Integer index = Math.min(components.size(), componentsToIndexMap.get(application.getName()));
+			components.add(index, new QanaryComponent(application, true));
+		}
+		components.add(new QanaryComponent(application, false));
+	}
+
+	public void removeComponent(Application application) {
+		components.removeIf(qanaryComponent -> qanaryComponent.getName().equals(application.getName()));
+	}
+
+	public int getPort() {
+		return this.port;
+	}
+
+	public String getHost() {
+		return this.host;
+	}
+
+	public URL getEndpoint() {
+		return this.endpoint;
+	}
 
 }
