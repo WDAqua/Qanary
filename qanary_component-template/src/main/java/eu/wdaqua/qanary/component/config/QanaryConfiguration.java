17a44a281d16d761072d5dd3cf7489be75299564
Andreas Both <andreas.both.de@gmail.com>
2016-04-13 13:33:16 +0200

    tiny update

---
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/config/QanaryConfiguration.java
@@ -0,0 +1,11 @@
+package eu.wdaqua.qanary.component.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QanaryConfiguration {
+	public static final String description = "/description";
+	public static final String annotatequestion = "/annotatequestion";
+	public static final String sparql = "/sparql";
+	public static final String questionRawDataUrlSuffix = "/raw";
+}
