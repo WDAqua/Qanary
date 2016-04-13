c3e33b621f660bd76c5d1ac625baf17daa89d1c8
Andreas Both <andreas.both.de@gmail.com>
2016-04-13 17:32:11 +0200

    cleanup

--- src/main/java/eu/wdaqua/qanary/component/QanaryService.java
+++ src/main/java/eu/wdaqua/qanary/component/QanaryService.java
@@ -4,16 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import eu.wdaqua.qanary.QanaryPipeline;
-//import eu.wdaqua.qanary.business.QanaryConfigurator;
-
 @SpringBootApplication
 @EnableAutoConfiguration
-// @RestController
 public class QanaryService {
-
-	// @Autowired
-	// public QanaryConfigurator configurator;
 
 	/**
 	 * default main, can be removed later
@@ -21,10 +14,6 @@ public class QanaryService {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		// Properties p = new Properties();
-		// new
-		// SpringApplicationBuilder(QanaryService.class).properties(p);//.run(args);
 		SpringApplication.run(QanaryService.class, args);
 	}
 
