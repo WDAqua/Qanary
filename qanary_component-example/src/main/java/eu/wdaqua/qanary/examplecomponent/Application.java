99754f4c8b4535f437163e5251b32fde64be4da7
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-04-14 18:04:45 +0200

    example component modifed like andreas said but not working

--- qanary_component-example/src/main/java/eu/wdaqua/qanary/examplecomponent/Application.java
+++ qanary_component-example/src/main/java/eu/wdaqua/qanary/examplecomponent/Application.java
@@ -3,7 +3,11 @@ package eu.wdaqua.qanary.examplecomponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import eu.wdaqua.qanary.component.QanaryComponent;
 import eu.wdaqua.qanary.component.QanaryService;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
@@ -12,9 +16,21 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan("eu.wdaqua.qanary.component")
+@ComponentScan("eu.wdaqua.qanary.examplecomponent")
 public class Application {
 
+	/**
+	* this method is needed to make the QanaryComponent in this project known
+	* to the QanaryServiceController in the qanary_component-template
+	* 
+	* @return
+	*/
+	@Bean
+	public QanaryComponent qanaryComponent() {
+		return new ExampleQanaryComponent();
+	}
+	
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
