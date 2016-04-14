b13bf293e7e17c2bf4c5a8fcfcfb6d604a18f7ae
Andreas Both <andreas.both.de@gmail.com>
2016-04-14 14:08:45 +0200

    removed comments for annotations
    now it should be possible to call the Stanford NLP component with an independent call like this:
    
    {code}
    curl -X POST -H "Accept: application/json" -H "Content-Type: application/json"   http://localhost:10001/annotatequestion -d '{"http://qanary/#endpoint":"http://admin:admin@localhost:5820/qanary/query","http://qanary/#inGraph":"urn:graph:018599a7-97e8-4507-b183-65da6de1a562","http://qanary/#outGraph":"urn:graph:018599a7-97e8-4507-b183-65da6de1a562"}' |  python -m json.tool
    {code}
    Note: The later pipe is just used for convenient output of the response data

--- qanary_component-stanford-NER/src/main/java/eu/wdaqua/qanary/StanfordNER/Application.java
+++ qanary_component-stanford-NER/src/main/java/eu/wdaqua/qanary/StanfordNER/Application.java
@@ -3,19 +3,17 @@ package eu.wdaqua.qanary.StanfordNER;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import eu.wdaqua.qanary.component.QanaryService;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by didier on 27.03.16.
  */
 @SpringBootApplication
-//@EnableAutoConfiguration
-//@ComponentScan("eu.wdaqua.qanary.component")
+@EnableAutoConfiguration
+@ComponentScan("eu.wdaqua.qanary.component")
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
