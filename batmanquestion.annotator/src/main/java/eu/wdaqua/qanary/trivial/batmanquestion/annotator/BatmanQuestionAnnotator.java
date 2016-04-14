5f9c8c8c2ab0b47d2b76df5cea9c768b05bf140f
Andreas Both <andreas.both@mercateo.com>
2016-04-14 14:08:27 +0200

    tiny changes

--- batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotator.java
+++ batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotator.java
@@ -1,8 +1,5 @@
 package eu.wdaqua.qanary.trivial.batmanquestion.annotator;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -23,14 +20,15 @@ public class BatmanQuestionAnnotator extends QanaryComponent {
 		logger.info("process: {}", myQanaryMessage);
 		logger.info("received: sparql Endpoint: {}, inGraph: {}, outGraph: {}", myQanaryMessage.getEndpoint(),
 				myQanaryMessage.getInGraph(), myQanaryMessage.getOutGraph());
-		// TODO: implement processing of question
+				// TODO: implement processing of question
 
-		try {
-			logger.info("store data in graph {}", myQanaryMessage.get(new URL(QanaryMessage.endpointKey)));
-			// TODO: insert data in QanaryMessage.outgraph
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
+		// try {
+		// logger.info("store data in graph {}", myQanaryMessage.get(new
+		// URL(QanaryMessage.endpointKey)));
+		// // TODO: insert data in QanaryMessage.outgraph
+		// } catch (MalformedURLException e) {
+		// e.printStackTrace();
+		// }
 
 		logger.info("apply vocabulary alignment on outgraph");
 		// TODO: implement this (custom for every component)
