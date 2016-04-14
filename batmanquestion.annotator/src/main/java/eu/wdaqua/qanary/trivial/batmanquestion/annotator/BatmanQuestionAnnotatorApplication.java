d5816b1da81c09adadfce85b0d0b232842302832
Andreas Both <andreas.both@mercateo.com>
2016-04-14 15:25:11 +0200

    this component now works as intended, one can use the following command to call the component externally
    example:
    {code}
    curl -X POST -H "Accept: application/json" -H "Content-Type: application/json"   http://localhost:54322/annotatequestion -d '{"http://qanary/#endpoint":"http://admin:admin@localhost:5820/qanary/query","http://qanary/#inGraph":"urn:graph:018599a7-97e8-4507-b183-65da6de1a562","http://qanary/#outGraph":"urn:graph:018599a7-97e8-4507-b183-65da6de1a562"}' |  python -m json.tool
    {code}

--- batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotatorApplication.java
+++ batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotatorApplication.java
@@ -3,7 +3,10 @@ package eu.wdaqua.qanary.trivial.batmanquestion.annotator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import eu.wdaqua.qanary.component.QanaryComponent;
 
 /**
  * start the spring application
@@ -15,6 +18,17 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ComponentScan("eu.wdaqua.qanary.component")
 public class BatmanQuestionAnnotatorApplication {
+
+	/**
+	 * this method is needed to make the QanaryComponent in this project known
+	 * to the QanaryServiceController in the qanary_component-template
+	 * 
+	 * @return
+	 */
+	@Bean
+	public QanaryComponent qanaryComponent() {
+		return new BatmanQuestionAnnotator();
+	}
 
 	public static void main(String... args) {
 		SpringApplication.run(BatmanQuestionAnnotatorApplication.class, args);
