4f6abf44f3af5c21d5c318e572297c4196a68fde
Kuldeep Singh <kulsingh@ram.local>
2016-04-15 23:24:39 +0200

    DBpediaSpotter- minor changes

--- qanary_component-spotter/src/main/java/eu/wdaqua/qanary/component/SpotlightWrapper2.java
+++ qanary_component-spotter/src/main/java/eu/wdaqua/qanary/component/SpotlightWrapper2.java
@@ -31,11 +31,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-//import edu.stanford.nlp.ling.CoreLabel;
-//import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-//import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-//import edu.stanford.nlp.pipeline.Annotation;
-//import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import eu.wdaqua.qanary.component.QanaryComponent;
 import eu.wdaqua.qanary.component.QanaryMessage;
 
@@ -83,18 +78,16 @@ public class SpotlightWrapper2 extends QanaryComponent {
 			//System.out.println("----------------------------");
 			boolean flg = true;
 			for (int temp = 0; temp < nList.getLength(); temp++) {
-				//System.out.println("Inside");
+				
 				Node nNode = nList.item(temp);
 				
-				//System.out.println("\nCurrent Element :" + nNode.getNodeName());
-
+				
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
 					Element eElement = (Element) nNode;
 					String text = eElement.getAttribute("name");
 					String offset = eElement.getAttribute("offset");
-					//System.out.println("Text : "    + text);
-					//System.out.println("Offset : "    + offset );
+
 					String startEnd = Integer.parseInt(offset)+","+(text.length()+Integer.parseInt(offset));
 					retLst.add(startEnd);
 				}
@@ -108,7 +101,8 @@ public class SpotlightWrapper2 extends QanaryComponent {
 	
 	public List<String> getResults(String input)
 	{
-		/*String SpotterService = "http://spotlight.sztaki.hu:2222/rest/spot";
+		/*This can be an alternative for passing text using API
+		 String SpotterService = "http://spotlight.sztaki.hu:2222/rest/spot";
 		UriComponentsBuilder service = UriComponentsBuilder.fromHttpUrl(SpotterService)
 		        .queryParam("text", qns);
 		logger.info("Service request "+service);
@@ -139,8 +133,7 @@ public class SpotlightWrapper2 extends QanaryComponent {
 		try {
 			
 			//STEP1: Retrive the named graph and the endpoint
-			//String endpoint=QanaryMessage.get(new URL(QanaryMessage.endpointKey)).toString();
-			//String namedGraph=QanaryMessage.get(new URL(QanaryMessage.inGraphKey)).toString();
+			
 			String endpoint = myQanaryMessage.getEndpoint().toASCIIString();
 		endpoint= "http://admin:admin@104.155.21.91:5820/qanary/query";
 		//http://admin:admin@localhost:5820/qanary/query
@@ -148,86 +141,17 @@ public class SpotlightWrapper2 extends QanaryComponent {
 			logger.info("store data at endpoint {}", endpoint);
 			logger.info("store data in graph {}", namedGraph);
 
-			//logger.info("store data at endpoint {}", endpoint);
-			//logger.info("store data in graph {}", namedGraph);
+			
 			
 			//STEP2: Retrive information that are needed for the computations
-			//TODO when "/question" is properly implemented and all things are loaded into the named graph 
-			// - The question
-			//String sparql = "PREFIX qa:<http://www.wdaqua.eu/qa#> "
-			//				+"SELECT ?questionuri "
-			//				+"FROM "+namedGraph+" "
-			//				+"WHERE {?questionuri a qa:Question}";
-			//ResultSet result=selectTripleStore(sparql,endpoint);
-			//String uriQuestion =result.next().getResource("questionuri").toString();
-			//logger.info("uri of the question {}", uriQuestion);
 			
-			//RestTemplate restTemplate = new RestTemplate();
-			//ResponseEntity<String> responseEntity =  restTemplate.getForEntity(questionuri, String.class);
-			//String question=responseEntity.getBody();
-			//logger.info("question {}", question);
 			
 			String uriQuestion="http://wdaqua.eu/dummy";
 			String question="Brooklyn Bridge was designed by Alfred";
 			
 			//STEP3: Pass the information to the component and execute it	
+			//logger.info("apply vocabulary alignment on outgraph");
 			
-			/*//TODO: ATTENTION: This should be done only ones when the component is started 
-				//Define the properties needed for the pipeline of the Stanford parser
-				Properties props = new Properties();
-				props.put("annotators", "tokenize, ssplit, pos, lemma, ner");
-				//Create a new pipline
-				StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-			
-			//Create an empty annotation just with the given text
-			Annotation document = new Annotation(question);
-			//Run the stanford annotator on question
-			pipeline.annotate(document);
-			
-			//Identify which parts of the question is tagged by the NER tool
-			//Go ones through the question, 
-			ArrayList<Selection> selections = new ArrayList<Selection>();
-			CoreLabel startToken=null; //stores the last token with non-zero tag, if it does not exist set to null
-			CoreLabel endToken=null; //stores the last found token with non-zero tag, if it does not exist set to null
-			//Iterate over the tags
-			for (CoreLabel token: document.get(TokensAnnotation.class)) {
-				logger.info("tagged question {}", token.toString()+token.get(NamedEntityTagAnnotation.class));
-	            //System.out.println(token.get(NamedEntityTagAnnotation.class));
-	            if (token.get(NamedEntityTagAnnotation.class).equals("O")==false){
-	            	if (startToken==null){
-	            		startToken=token;
-	            		endToken=token;
-	            	} else {
-	            		if (startToken.get(NamedEntityTagAnnotation.class)==token.get(NamedEntityTagAnnotation.class)){
-	            			endToken=token;
-	            		} else {
-	            			Selection s = new Selection();
-	            			s.begin=startToken.beginPosition();
-	                		s.end=endToken.endPosition();
-	                		selections.add(s);
-	                		startToken=token;
-	            		}
-	            	}
-	            } else {
-	            	if (startToken!=null){ 
-	            		Selection s = new Selection();
-	            		s.begin=startToken.beginPosition();
-	            		s.end=endToken.endPosition();
-	            		selections.add(s);
-	            		startToken=null;
-	            		endToken=null;
-	            	}
-	            }
-			}
-			if (startToken!=null){ 
-	    		Selection s = new Selection();
-	    		s.begin=startToken.beginPosition();
-	    		s.end=endToken.endPosition();
-	    		selections.add(s);
-	    	}
-			
-			logger.info("apply vocabulary alignment on outgraph");
-			*/
 			SpotlightWrapper2 qaw = new SpotlightWrapper2();
 			
 			List<String > stEn = new ArrayList<String>();
@@ -285,18 +209,6 @@ public class SpotlightWrapper2 extends QanaryComponent {
 		UpdateProcessor proc = UpdateExecutionFactory.createRemote(request, endpoint);
 	    proc.execute();
 	}
-	/*
-	public ResultSet selectTripleStore(String sparqlQuery, String endpoint){
-		Query query = QueryFactory.create(sparqlQuery);
-		QueryExecution qExe = QueryExecutionFactory.sparqlService(endpoint, query);
-		return qExe.execSelect();
-	}
-	
-	public void loadTripleStore(String sparqlQuery, String endpoint) {
-		UpdateRequest request = UpdateFactory.create(sparqlQuery);
-		UpdateProcessor proc = UpdateExecutionFactory.createRemote(request, endpoint);
-		proc.execute();
-	}*/
 
 	public ResultSet selectTripleStore(String sparqlQuery, String endpoint) {
 		Query query = QueryFactory.create(sparqlQuery);
