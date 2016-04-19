93f914e9541563f738e0816852fa5bced36e1d12
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-04-19 14:24:23 +0200

    Evaluation tool against qald-6

---
+++ qanary_evaluation_tool/src/test/java/TestQanaryServiceController/TestMain.java
@@ -0,0 +1,18 @@
+package TestQanaryServiceController;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import eu.wdaqua.qanary.evaluationTool.Evaluation;
+
+public class TestMain {
+
+	@Test
+	public void test() {
+		Evaluation e = new Evaluation();
+		e.qald6_test();
+		fail("Not yet implemented");
+	}
+
+}
