1b344e1e1656b9a8af861c265215dac513855ff9
Andreas Both <andreas.both.de@gmail.com>
2016-04-19 02:44:08 +0200

    New method QanaryComponent::selectTripleStore does not work, because of unsolved dependency problem.
    SLF4J: Class path contains multiple SLF4J bindings.
    ...
    SLF4J: See http://www.slf4j.org/codes.html#multiple_bindings for an explanation.
    SLF4J: Detected both log4j-over-slf4j.jar AND bound slf4j-log4j12.jar on the class path, preempting StackOverflowError.
    SLF4J: See also http://www.slf4j.org/codes.html#log4jDelegationLoop for more details.
    Exception in thread "main" java.lang.ExceptionInInitializerError

--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryComponent.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryComponent.java
@@ -3,6 +3,13 @@ package eu.wdaqua.qanary.component;
 import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
+//import org.apache.jena.query.Query;
+//import org.apache.jena.query.QueryExecution;
+//import org.apache.jena.query.QueryExecutionFactory;
+//import org.apache.jena.query.QueryFactory;
+//import org.apache.jena.query.ResultSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * represent the behavior of an annotator following the Qanary methodology
@@ -11,6 +18,8 @@ import org.apache.http.client.ClientProtocolException;
  *
  */
 public abstract class QanaryComponent {
+
+	private static final Logger logger = LoggerFactory.getLogger(QanaryComponent.class);
 
 	// TODO need to be changed
 	final String questionUrl = "http://localhost:8080/question/28f56d32-b30a-428d-ac90-79372a6f7625/";
@@ -21,7 +30,7 @@ public abstract class QanaryComponent {
 	 * @param myQanaryMessage
 	 * @return
 	 */
-	public abstract QanaryMessage process(QanaryMessage myQanaryMessage);
+	public abstract QanaryMessage process(QanaryMessage myQanaryMessage) throws Exception;
 
 	/**
 	 * fetch raw data for a question
@@ -60,5 +69,62 @@ public abstract class QanaryComponent {
 
 		return null;
 	}
+
+	/**
+	 * query a SPARQL endpoint with a given query
+	 * 
+	 * @param sparqlQuery
+	 * @param endpoint
+	 * @return
+	 */
+	// public ResultSet selectTripleStore(String sparqlQuery, String endpoint) {
+	// logger.debug("selectTripleStore on {} execute {}", endpoint,
+	// sparqlQuery);
+	// Query query = QueryFactory.create(sparqlQuery);
+	// QueryExecution qExe = QueryExecutionFactory.sparqlService(endpoint,
+	// query);
+	// ResultSet resultset = qExe.execSelect();
+	// return resultset;
+	// }
+
+	/**
+	 * get the question URI from the pipeline endpoint
+	 * 
+	 * @param myQanaryMessage
+	 * @return
+	 * @throws Exception
+	 */
+	// public URI getQuestion(QanaryMessage myQanaryMessage) throws Exception {
+	// ResultSet resultset = this.selectTripleStore(
+	// "SELECT ?question FROM <" + myQanaryMessage.getInGraph()
+	// + "> {?question <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>
+	// <http://www.wdaqua.eu/qa#Question>}",
+	// myQanaryMessage.getEndpoint().toString());
+	//
+	// int i = 0;
+	// String question = null;
+	// while (resultset.hasNext()) {
+	// question = resultset.next().get("question").asResource().getURI();
+	// logger.debug("{}: qa#Question = {}", i++, question);
+	// }
+	// if (i > 1) {
+	// throw new Exception("More than 1 question (count: " + i + ") in graph " +
+	// myQanaryMessage.getInGraph()
+	// + " at " + myQanaryMessage.getEndpoint());
+	// } else if (i == 0) {
+	// throw new Exception("No question available in graph " +
+	// myQanaryMessage.getInGraph() + " at "
+	// + myQanaryMessage.getEndpoint());
+	// }
+	//
+	// URI questionUri = new URI(question);
+	//
+	// logger.info("question {} found in {} at {}", question,
+	// myQanaryMessage.getInGraph(),
+	// myQanaryMessage.getEndpoint());
+	//
+	// return questionUri;
+	//
+	// }
 
 }
