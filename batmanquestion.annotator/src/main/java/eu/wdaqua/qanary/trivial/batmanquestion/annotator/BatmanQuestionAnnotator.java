5dac410cf1038546d884b83278bdd955ac50df91
Andreas Both <andreas.both.de@gmail.com>
2016-04-19 02:27:06 +0200

    created a method for querying the RDF from an endpoint, wrapper for querying the question URI from the endpoint both need to be moved to QanaryComponent

--- batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotator.java
+++ batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotator.java
@@ -1,5 +1,12 @@
 package eu.wdaqua.qanary.trivial.batmanquestion.annotator;
 
+import java.net.URI;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,9 +21,11 @@ public class BatmanQuestionAnnotator extends QanaryComponent {
 	/**
 	 * implement this method encapsulating the functionality of your Qanary
 	 * component
+	 * 
+	 * @throws Exception
 	 */
 	@Override
-	public QanaryMessage process(QanaryMessage myQanaryMessage) {
+	public QanaryMessage process(QanaryMessage myQanaryMessage) throws Exception {
 
 		logger.info("process: {}", myQanaryMessage);
 		logger.info("received: sparql Endpoint: {}, inGraph: {}, outGraph: {}", myQanaryMessage.getEndpoint(),
@@ -34,7 +43,63 @@ public class BatmanQuestionAnnotator extends QanaryComponent {
 		logger.info("apply vocabulary alignment on outgraph for the Batman question");
 		// TODO: implement this (custom for every component)
 
+		this.getQuestion(myQanaryMessage);
+
+		// logger.debug("SELECT * FROM <" + myQanaryMessage.getInGraph() + ">
+		// {?s ?p ?o }");
+
 		return myQanaryMessage;
+	}
+
+	/**
+	 * query a SPARQL endpoint with a given query
+	 * 
+	 * @param sparqlQuery
+	 * @param endpoint
+	 * @return
+	 */
+	public ResultSet selectTripleStore(String sparqlQuery, String endpoint) {
+		logger.debug("selectTripleStore on {} execute {}", endpoint, sparqlQuery);
+		Query query = QueryFactory.create(sparqlQuery);
+		QueryExecution qExe = QueryExecutionFactory.sparqlService(endpoint, query);
+		ResultSet resultset = qExe.execSelect();
+		return resultset;
+	}
+
+	/**
+	 * get the question URI from the pipeline endpoint
+	 * 
+	 * @param myQanaryMessage
+	 * @return
+	 * @throws Exception
+	 */
+	public URI getQuestion(QanaryMessage myQanaryMessage) throws Exception {
+		ResultSet resultset = this.selectTripleStore(
+				"SELECT ?question FROM <" + myQanaryMessage.getInGraph()
+						+ "> {?question <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.wdaqua.eu/qa#Question>}",
+				myQanaryMessage.getEndpoint().toString());
+
+		int i = 0;
+		String question = null;
+		while (resultset.hasNext()) {
+			question = resultset.next().get("question").asResource().getURI();
+			logger.debug("{}: qa#Question = {}", i++, question);
+		}
+		if (i > 1) {
+			throw new Exception("More than 1 question (count: " + i + ") in graph " + myQanaryMessage.getInGraph()
+					+ " at " + myQanaryMessage.getEndpoint());
+		} else if (i == 0) {
+			throw new Exception("No question available in graph " + myQanaryMessage.getInGraph() + " at "
+					+ myQanaryMessage.getEndpoint());
+		}
+
+		URI questionUri = new URI(question);
+
+		logger.info("question {} found in {} at {}", question, myQanaryMessage.getInGraph(),
+				myQanaryMessage.getEndpoint());
+
+		return questionUri;
+
 	}
 
 }
