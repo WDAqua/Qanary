75b840cc6286d265d9c910827dc456e98991f3c9
Andreas Both <andreas.both.de@gmail.com>
2016-04-22 02:38:24 +0200

    moved utility methods to QanaryUtils accessible via QanaryComponent (hence in each component derived from QanaryComponent); introduces QanaryQuestion encapsulating access to the question URI and raw data

--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryQuestion.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryQuestion.java
@@ -1,5 +1,34 @@
 package eu.wdaqua.qanary.component;
 
-public class QanaryQuestion {
+import java.net.URI;
+
+public class QanaryQuestion<T> {
+
+	private final URI uri;
+	private T raw;
+
+	public QanaryQuestion(URI questionUri) {
+		this.uri = questionUri;
+	}
+
+	/**
+	 * returns the URI of the question
+	 * 
+	 * @return
+	 */
+	public URI getUri() {
+		return this.uri;
+	}
+
+	/**
+	 * returns the raw data of the question fetched from the URI provided via
+	 * the constructor, the result is cached to prevent unnecessary calls to
+	 * remote services
+	 *
+	 * @return
+	 */
+	public T getRawData() {
+		return this.raw;
+	}
 
 }
