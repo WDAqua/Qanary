8d6283d0cac65fffac74fd7094176dc667fe6d7e
Andreas Both <anbo-de>
2016-04-23 00:54:59 +0200

    tiny improvements and comments

--- qanary_component-dbpediaspotter/src/main/java/eu/wdaqua/qanary/DbpediaSpotlightSpotter/WrapperSpotlight.java
+++ qanary_component-dbpediaspotter/src/main/java/eu/wdaqua/qanary/DbpediaSpotlightSpotter/WrapperSpotlight.java
@@ -1,14 +1,11 @@
 package eu.wdaqua.qanary.DbpediaSpotlightSpotter;
 
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-import java.util.Map.Entry;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -45,15 +42,16 @@ import eu.wdaqua.qanary.component.QanaryMessage;
 @Component
 public class WrapperSpotlight extends QanaryComponent {
 	private static final Logger logger = LoggerFactory.getLogger(WrapperSpotlight.class);
+
 	/**
 	 * default processor of a QanaryMessage
 	 */
-	
+
 	public List<String> usingXml(String urladd) {
 		String urladdress = "";
 		List<String> retLst = new ArrayList<String>();
 		try {
-			
+
 			URL url = new URL(urladd);
 			URLConnection urlConnection = url.openConnection();
 			HttpURLConnection connection = null;
@@ -63,8 +61,7 @@ public class WrapperSpotlight extends QanaryComponent {
 				System.out.println("Please enter an HTTP URL.");
 				return retLst;
 			}
-			
-			
+
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(connection.getInputStream());
@@ -72,22 +69,20 @@ public class WrapperSpotlight extends QanaryComponent {
 			doc.getDocumentElement().normalize();
 
 			NodeList nList = doc.getElementsByTagName("surfaceForm");
-			
-			
-			//System.out.println("----------------------------");
+
+			// System.out.println("----------------------------");
 			boolean flg = true;
 			for (int temp = 0; temp < nList.getLength(); temp++) {
-				
+
 				Node nNode = nList.item(temp);
-				
-				
+
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
 					Element eElement = (Element) nNode;
 					String text = eElement.getAttribute("name");
 					String offset = eElement.getAttribute("offset");
 
-					String startEnd = Integer.parseInt(offset)+","+(text.length()+Integer.parseInt(offset));
+					String startEnd = Integer.parseInt(offset) + "," + (text.length() + Integer.parseInt(offset));
 					retLst.add(startEnd);
 				}
 			}
@@ -96,136 +91,126 @@ public class WrapperSpotlight extends QanaryComponent {
 		}
 		return retLst;
 	}
-	
-	
-	public List<String> getResults(String input)
-	{
-		/*This can be an alternative for passing text using API
-		 String SpotterService = "http://spotlight.sztaki.hu:2222/rest/spot";
-		UriComponentsBuilder service = UriComponentsBuilder.fromHttpUrl(SpotterService)
-		        .queryParam("text", qns);
-		logger.info("Service request "+service);
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.exchange(service.build().encode().toUri(), HttpMethod.GET, null, String.class);
-		logger.info("Xml document from alchemy api {}", response.getBody());
-		*/
-	
+
+	public List<String> getResults(String input) {
+		/*
+		 * This can be an alternative for passing text using API String
+		 * SpotterService = "http://spotlight.sztaki.hu:2222/rest/spot";
+		 * UriComponentsBuilder service =
+		 * UriComponentsBuilder.fromHttpUrl(SpotterService) .queryParam("text",
+		 * qns); logger.info("Service request "+service); RestTemplate
+		 * restTemplate = new RestTemplate(); ResponseEntity<String> response =
+		 * restTemplate.exchange(service.build().encode().toUri(),
+		 * HttpMethod.GET, null, String.class); logger.info(
+		 * "Xml document from alchemy api {}", response.getBody());
+		 */
+
+		// TODO: Should move to the config
 		String madeUrlFromInput = "http://spotlight.sztaki.hu:2222/rest/spot?text=";
-		/*String qns[] = input.split(" ");
-		String append = String.join("%20", qns);*/
-		try{
-		madeUrlFromInput += URLEncoder.encode(input, "UTF-8");;//+"&executeSparqlQuery=on&relationExtractorType=Semantic";
-		}catch(Exception e){}
+		/*
+		 * String qns[] = input.split(" "); String append = String.join("%20",
+		 * qns);
+		 */
+		try {
+			madeUrlFromInput += URLEncoder.encode(input, "UTF-8");
+			;// +"&executeSparqlQuery=on&relationExtractorType=Semantic";
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.warn(e.getMessage());
+		}
+
 		List<String> retLst = new ArrayList<String>();
 		{
-			
-			System.out.println("URL is: "+madeUrlFromInput);
+			logger.info("URL is: {}", madeUrlFromInput);
 			retLst = usingXml(madeUrlFromInput);
 		}
-		
+
 		return retLst;
 	}
-	
+
 	public QanaryMessage process(QanaryMessage myQanaryMessage) {
 		long startTime = System.currentTimeMillis();
 		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
 		logger.info("Qanary Message: {}", myQanaryMessage);
 
-		
 		try {
-			
-			//STEP1: Retrive the named graph and the endpoint
-			
-			//String endpoint = myQanaryMessage.getEndpoint().toASCIIString();
-			//endpoint= "http://admin:admin@104.155.21.91:5820/qanary/query";
-			//http://admin:admin@localhost:5820/qanary/query
-			//String namedGraph = myQanaryMessage.getInGraph().toASCIIString();
-			//logger.info("store data at endpoint {}", endpoint);
-			//logger.info("store data in graph {}", namedGraph);
 
-			
+			// STEP1: Retrieve the named graph and the endpoint
+
+			// String endpoint = myQanaryMessage.getEndpoint().toASCIIString();
+			// endpoint= "http://admin:admin@104.155.21.91:5820/qanary/query";
+			// http://admin:admin@localhost:5820/qanary/query
+			// String namedGraph = myQanaryMessage.getInGraph().toASCIIString();
+			// logger.info("store data at endpoint {}", endpoint);
+			// logger.info("store data in graph {}", namedGraph);
+
 			String endpoint = myQanaryMessage.getEndpoint().toASCIIString();
 			String namedGraph = myQanaryMessage.getInGraph().toASCIIString();
 			logger.info("Endpoint: {}", endpoint);
 			logger.info("InGraph: {}", namedGraph);
 
-			//STEP2: Retrive information that are needed for the computations
-			String sparql = "PREFIX qa:<http://www.wdaqua.eu/qa#> "
-					+"SELECT ?questionuri "
-					+"FROM <"+namedGraph+"> "
-					+"WHERE {?questionuri a qa:Question}";
-				ResultSet result=selectTripleStore(sparql,endpoint);
-				String uriQuestion=result.next().getResource("questionuri").toString();
-				logger.info("Uri of the question: {}", uriQuestion);
-				//Retrive the question itself
-				RestTemplate restTemplate = new RestTemplate();
-				//TODO: pay attention to "/raw" maybe change that
-				ResponseEntity<String> responseEntity = restTemplate.getForEntity(uriQuestion+"/raw", String.class);
-				String question=responseEntity.getBody();
-				logger.info("Question: {}", question);
-			
-			//String uriQuestion="http://wdaqua.eu/dummy";
-			//String question="Brooklyn Bridge was designed by Alfred";
-			
-			//STEP3: Pass the information to the component and execute it	
-			//logger.info("apply vocabulary alignment on outgraph");
-			
+			// STEP2: Retriexve information that are needed for the computations
+			String sparql = "PREFIX qa:<http://www.wdaqua.eu/qa#> " + "SELECT ?questionuri " + "FROM <" + namedGraph
+					+ "> " + "WHERE {?questionuri a qa:Question}";
+			ResultSet result = selectTripleStore(sparql, endpoint);
+			String uriQuestion = result.next().getResource("questionuri").toString();
+			logger.info("Uri of the question: {}", uriQuestion);
+			// Retrive the question itself
+			RestTemplate restTemplate = new RestTemplate();
+			// TODO: pay attention to "/raw" maybe change that
+			ResponseEntity<String> responseEntity = restTemplate.getForEntity(uriQuestion + "/raw", String.class);
+			String question = responseEntity.getBody();
+			logger.info("Question: {}", question);
+
+			// String uriQuestion="http://wdaqua.eu/dummy";
+			// String question="Brooklyn Bridge was designed by Alfred";
+
+			// STEP3: Pass the information to the component and execute it
+			// logger.info("apply vocabulary alignment on outgraph");
+
 			WrapperSpotlight qaw = new WrapperSpotlight();
-			
-			List<String > stEn = new ArrayList<String>();
-			stEn  = qaw.getResults(question);
+
+			List<String> stEn = new ArrayList<String>();
+			stEn = qaw.getResults(question);
 			int cnt = 0;
 			ArrayList<Selection> selections = new ArrayList<Selection>();
-			for(String str: stEn)
-			{
+			for (String str : stEn) {
 				Selection s1 = new Selection();
 				String str1[] = str.split(",");
 				s1.begin = Integer.parseInt(str1[0]);
 				s1.end = Integer.parseInt(str1[1]);
 				selections.add(s1);
 			}
-			//STEP4: Push the result of the component to the triplestore
-			
-			for (Selection s: selections){
-				sparql="prefix qa: <http://www.wdaqua.eu/qa#> "
-						 +"prefix oa: <http://www.w3.org/ns/openannotation/core/> "
-						 +"prefix xsd: <http://www.w3.org/2001/XMLSchema#> "
-						 +"INSERT { "
-						 +"GRAPH <"+namedGraph+"> { "
-						 +"  ?a a qa:AnnotationOfNamedEntity . "
-						 +"  ?a oa:hasTarget [ "
-						 +"           a    oa:SpecificResource; "
-						 +"           oa:hasSource    <"+uriQuestion+">; "
-						 +"           oa:hasSelector  [ "
-						 +"                    a oa:TextPositionSelector ; "
-						 +"                    oa:start \""+s.begin+"\"^^xsd:nonNegativeInteger ; "
-						 +"                    oa:end  \""+s.end+"\"^^xsd:nonNegativeInteger  "
-						 +"           ] "
-						 +"  ] ; "
-						 +"     oa:annotatedBy <http://spotlight.sztaki.hu:2222/rest/spot> ; "
-						 +"	    oa:AnnotatedAt ?time  "
-						 +"}} "
-						 +"WHERE { " 
-						 +"BIND (IRI(str(RAND())) AS ?a) ."
-						 +"BIND (now() as ?time) "
-					     +"}";
+			// STEP4: Push the result of the component to the triplestore
+
+			for (Selection s : selections) {
+				sparql = "prefix qa: <http://www.wdaqua.eu/qa#> "
+						+ "prefix oa: <http://www.w3.org/ns/openannotation/core/> "
+						+ "prefix xsd: <http://www.w3.org/2001/XMLSchema#> " + "INSERT { " + "GRAPH <" + namedGraph
+						+ "> { " + "  ?a a qa:AnnotationOfNamedEntity . " + "  ?a oa:hasTarget [ "
+						+ "           a    oa:SpecificResxource; " + "           oa:hasSource    <" + uriQuestion
+						+ ">; " + "           oa:hasSelector  [ " + "                    a oa:TextPositionSelector ; "
+						+ "                    oa:start \"" + s.begin + "\"^^xsd:nonNegativeInteger ; "
+						+ "                    oa:end  \"" + s.end + "\"^^xsd:nonNegativeInteger  " + "           ] "
+						+ "  ] ; " + "     oa:annotatedBy <http://spotlight.sztaki.hu:2222/rest/spot> ; "
+						+ "	    oa:AnnotatedAt ?time  " + "}} " + "WHERE { " + "BIND (IRI(str(RAND())) AS ?a) ."
+						+ "BIND (now() as ?time) " + "}";
 				loadTripleStore(sparql, endpoint);
 			}
 			long estimatedTime = System.currentTimeMillis() - startTime;
-			logger.info("Time {}", estimatedTime);		
+			logger.info("Time {}", estimatedTime);
 
-		} catch (Exception e) {//MalformedURLException e) {
+		} catch (Exception e) {// MalformedURLException e) {
 			e.printStackTrace();
 		}
 
 		return myQanaryMessage;
 	}
-	
-	
+
 	public void loadTripleStore(String sparqlQuery, String endpoint) {
-	    UpdateRequest request = UpdateFactory.create(sparqlQuery);
+		UpdateRequest request = UpdateFactory.create(sparqlQuery);
 		UpdateProcessor proc = UpdateExecutionFactory.createRemote(request, endpoint);
-	    proc.execute();
+		proc.execute();
 	}
 
 	public ResultSet selectTripleStore(String sparqlQuery, String endpoint) {
@@ -238,6 +223,5 @@ public class WrapperSpotlight extends QanaryComponent {
 		public int begin;
 		public int end;
 	}
-	
-}
 
+}
