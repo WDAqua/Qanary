567add1deedc971220136eeb67f0d38246fdc0a4
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-04-22 17:57:41 +0200

    FOX as a component added

---
+++ qanary_component-FOX/src/main/java/eu/wdaqua/qanary/FOX/Application.java
@@ -0,0 +1,32 @@
+package eu.wdaqua.qanary.FOX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import eu.wdaqua.qanary.component.QanaryComponent;
+
+/**
+ * Created by didier on 27.03.16.
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("eu.wdaqua.qanary.component")
+public class Application {
+	/**
+	 * this method is needed to make the QanaryComponent in this project known
+	 * to the QanaryServiceController in the qanary_component-template
+	 * 
+	 * @return
+	 */
+	@Bean
+	public QanaryComponent qanaryComponent() {
+		return new FOXComponent();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
