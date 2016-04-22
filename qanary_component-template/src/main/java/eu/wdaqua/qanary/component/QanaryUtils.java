9d8cb21fe3d5a0863b23d5edc921424957b13db8
Andreas Both <andreas.both.de@gmail.com>
2016-04-22 18:02:53 +0200

    new utils class

---
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryUtils.java
@@ -0,0 +1,124 @@
+package eu.wdaqua.qanary.component;
+
+import java.net.URI;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * the class is a covering standard tasks users of the Qanary methodology might
+ * have
+ * 
+ * @author AnBo
+ *
+ */
+public class QanaryUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(QanaryUtils.class);
+
+	private final URI endpoint;
+	private final URI inGraph;
+	private final URI outGraph;
+
+	QanaryUtils(QanaryMessage qanaryMessage) {
+		this.endpoint = qanaryMessage.getEndpoint();
+		this.inGraph = qanaryMessage.getInGraph();
+		this.outGraph = qanaryMessage.getOutGraph();
+	}
+
+	/**
+	 * returns the endpoint provided by the QanaryMessage object provided via
+	 * constructor
+	 * 
+	 * @return
+	 */
+	public URI getEndpoint() {
+		return this.endpoint;
+	}
+
+	/**
+	 * returns the inGraph provided by the QanaryMessage object provided via
+	 * constructor
+	 *
+	 * @return
+	 */
+	public URI getInGraph() {
+		return this.inGraph;
+	}
+
+	/**
+	 * returns the outGraph provided by the QanaryMessage object provided via
+	 * constructor
+	 *
+	 * @return
+	 */
+	public URI getOutGraph() {
+		return this.outGraph;
+	}
+
+	/**
+	 * wrapper for selectTripleStore
+	 * 
+	 * @param sparqlQuery
+	 * @param qanaryMessage
+	 * @return
+	 */
+	public ResultSet selectFromTripleStore(String sparqlQuery) {
+		return this.selectFromTripleStore(sparqlQuery, this.getEndpoint().toString());
+	}
+
+	/**
+	 * query a SPARQL endpoint with a given query
+	 * 
+	 * @param sparqlQuery
+	 * @param endpoint
+	 * @return
+	 */
+	public ResultSet selectFromTripleStore(String sparqlQuery, String endpoint) {
+		logger.debug("selectTripleStore on {} execute {}", endpoint, sparqlQuery);
+		Query query = QueryFactory.create(sparqlQuery);
+		QueryExecution qExe = QueryExecutionFactory.sparqlService(endpoint, query);
+		ResultSet resultset = qExe.execSelect();
+		return resultset;
+	}
+
+	/**
+	 * get the question URI from the pipeline endpoint
+	 * 
+	 * @param myQanaryMessage
+	 * @return
+	 * @throws Exception
+	 */
+	public QanaryQuestion getQuestion() throws Exception {
+		ResultSet resultset = this.selectFromTripleStore(
+				"SELECT ?question FROM <" + this.getInGraph()
+						+ "> {?question <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.wdaqua.eu/qa#Question>}",
+				this.getEndpoint().toString());
+
+		int i = 0;
+		String question = null;
+		while (resultset.hasNext()) {
+			question = resultset.next().get("question").asResource().getURI();
+			logger.debug("{}: qa#Question = {}", i++, question);
+		}
+		if (i > 1) {
+			throw new Exception("More than 1 question (count: " + i + ") in graph " + this.getInGraph() + " at "
+					+ this.getEndpoint());
+		} else if (i == 0) {
+			throw new Exception("No question available in graph " + this.getInGraph() + " at " + this.getEndpoint());
+		}
+
+		URI questionUri = new URI(question);
+
+		logger.info("question {} found in {} at {}", question, this.getInGraph(), this.getEndpoint());
+
+		return new QanaryQuestion<Object>(questionUri);
+
+	}
+
+}
