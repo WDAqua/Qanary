a31e67134fab91b18c8803b9f4d7910f253acfae
Andreas Both <andreas.both.de@gmail.com>
2016-04-23 02:40:25 +0200

    introduced utility methods - QanaryUtils::getTime for supporting time measurements - QanaryUtils::logTime for standard time logging of runtimes - QanaryUtils::updateTripleStore for INSERT queries to the triplestore - QanaryUtils::addAnnotations for adding entity annotations to the triplestore - added time measurement to the SPARQL queries and the QanaryServiceController::annotatequestion, hence the runtime of all standard functionality is not trackable (LOGLEVEL: debug)

--- src/main/java/eu/wdaqua/qanary/component/ontology/TextPositionSelector.java
+++ src/main/java/eu/wdaqua/qanary/component/ontology/TextPositionSelector.java
@@ -1,8 +1,10 @@
 package eu.wdaqua.qanary.component.ontology;
 
+import org.apache.jena.reasoner.IllegalParameterException;
+
 /**
  * represents the textpositionselector concept of the W3C OpenAnnotation
- * vocabulary
+ * vocabulary {@linktourl https://www.w3.org/ns/oa#d4e667}
  * 
  * @author AnBo
  *
@@ -11,7 +13,20 @@ public class TextPositionSelector {
 	private final int start;
 	private final int end;
 
-	public TextPositionSelector(int start, int end) {
+	public TextPositionSelector(int start, int end) throws IllegalParameterException {
+		if (start < 0) {
+			throw new IllegalParameterException(
+					"start was lower than 0, selections have to be positive (was " + start + ").");
+		}
+		if (end < 0) {
+			throw new IllegalParameterException(
+					"end was lower than 0, selections have to be positive (was " + end + ").");
+		}
+		if (end - start < 0) {
+			throw new IllegalParameterException(
+					"calculation (start - end) was lower than 0, selections have to cover a positive text range (provided indexes: "
+							+ start + ", " + end + ").");
+		}
 		this.start = start;
 		this.end = end;
 	}
