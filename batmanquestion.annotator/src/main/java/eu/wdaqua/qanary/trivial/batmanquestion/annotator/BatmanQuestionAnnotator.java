c3676a6d5356afac19664e8f9b296040dff11eed
Andreas Both <andreas.both.de@gmail.com>
2016-04-23 02:45:33 +0200

    the examplary component for processing the Batman question of QALD-6 does now create actual correct annotations in the triplestore using standard functionality (of QanaryComponent)

--- batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotator.java
+++ batmanquestion.annotator/src/main/java/eu/wdaqua/qanary/trivial/batmanquestion/annotator/BatmanQuestionAnnotator.java
@@ -1,5 +1,8 @@
 package eu.wdaqua.qanary.trivial.batmanquestion.annotator;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,10 +11,20 @@ import eu.wdaqua.qanary.component.QanaryComponent;
 import eu.wdaqua.qanary.component.QanaryMessage;
 import eu.wdaqua.qanary.component.QanaryQuestion;
 import eu.wdaqua.qanary.component.QanaryUtils;
+import eu.wdaqua.qanary.component.ontology.TextPositionSelector;
 
+/**
+ * represents the behavior of an annotator of QALD-6 question number 184:
+ * "Who created Batman?", i.e., if this question is provided, then it is
+ * annotated with the entities (only this question!)
+ * 
+ * @author AnBo
+ *
+ */
 @Component
 public class BatmanQuestionAnnotator extends QanaryComponent {
 	private static final Logger logger = LoggerFactory.getLogger(BatmanQuestionAnnotator.class);
+	final String questionToBeAccepted = "Who created Batman?";
 
 	/**
 	 * implement this method encapsulating the functionality of your Qanary
@@ -32,11 +45,33 @@ public class BatmanQuestionAnnotator extends QanaryComponent {
 		// TODO: wait for the fully working implementation in QanaryComponent
 		QanaryUtils utils = this.getUtils(myQanaryMessage);
 		QanaryQuestion<String> question = utils.getQuestion();
-		logger.info("question: {} from {}", question.getUri(), question.getRawData());
+		logger.info("question: {} from {}", question.getRawData(), question.getUri());
 
-		logger.info("apply vocabulary alignment on outgraph for the Batman question");
+		if (question.getRawData().toLowerCase().compareTo(questionToBeAccepted.toLowerCase()) == 0) {
+			logger.info("question recognized that can be processed: {}", question.getRawData());
+			logger.info("apply vocabulary alignment on outgraph for the Batman question");
+			this.annotateBatmanQuestionWithPredefinedEntities(utils);
+		} else {
+			logger.warn("question \"{}\" cannot be processed from this exemplary component, only \"{}\" is accepted.",
+					question.getRawData(), this.questionToBeAccepted);
+		}
 
 		return myQanaryMessage;
+	}
+
+	/**
+	 * annotate two entities as expected by QALD-6: "Who created Batman?"
+	 * (created, Batman)
+	 * 
+	 * @throws Exception
+	 */
+	private void annotateBatmanQuestionWithPredefinedEntities(QanaryUtils utils) throws Exception {
+		List<TextPositionSelector> selectors = new LinkedList<>();
+
+		selectors.add(new TextPositionSelector(4, 10));
+		selectors.add(new TextPositionSelector(12, 17));
+
+		utils.addAnnotations(selectors);
 	}
 
 }
