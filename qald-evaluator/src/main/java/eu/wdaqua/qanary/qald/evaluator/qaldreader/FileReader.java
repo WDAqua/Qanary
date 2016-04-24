0da8cf207c38f2125a99d8cee16b1b67abb75572
Andreas Both <andreas.both.de@gmail.com>
2016-04-24 18:06:27 +0200

    new approach for QALD evaluation tool for Qanary driven QA systems

---
+++ qald-evaluator/src/main/java/eu/wdaqua/qanary/qald/evaluator/qaldreader/FileReader.java
@@ -0,0 +1,207 @@
+package eu.wdaqua.qanary.qald.evaluator.qaldreader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.sparql.algebra.Algebra;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.core.BasicPattern;
+import org.apache.jena.sparql.syntax.Element;
+import org.apache.jena.sparql.syntax.ElementAssign;
+import org.apache.jena.sparql.syntax.ElementBind;
+import org.apache.jena.sparql.syntax.ElementData;
+import org.apache.jena.sparql.syntax.ElementDataset;
+import org.apache.jena.sparql.syntax.ElementExists;
+import org.apache.jena.sparql.syntax.ElementFilter;
+import org.apache.jena.sparql.syntax.ElementGroup;
+import org.apache.jena.sparql.syntax.ElementMinus;
+import org.apache.jena.sparql.syntax.ElementNamedGraph;
+import org.apache.jena.sparql.syntax.ElementNotExists;
+import org.apache.jena.sparql.syntax.ElementOptional;
+import org.apache.jena.sparql.syntax.ElementPathBlock;
+import org.apache.jena.sparql.syntax.ElementService;
+import org.apache.jena.sparql.syntax.ElementSubQuery;
+import org.apache.jena.sparql.syntax.ElementTriplesBlock;
+import org.apache.jena.sparql.syntax.ElementUnion;
+import org.apache.jena.sparql.syntax.ElementVisitor;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+public class FileReader {
+
+	public void readFile() throws UnsupportedEncodingException, IOException {
+		Reader reader = new InputStreamReader(FileReader.class.getResourceAsStream("/qald-6-test-multilingual.json"),
+				"UTF-8");
+		Gson gson = new GsonBuilder().create();
+		JsonObject json = gson.fromJson(reader, JsonObject.class);
+
+		JsonArray questions = json.get("questions").getAsJsonArray();
+		// System.out.println(questions);
+		System.out.println("size: " + questions.size());
+
+		JsonObject question;
+		JsonArray questiondata;
+		String language;
+		String questionstring;
+		JsonObject query;
+		String sparqlquery;
+
+		for (int i = 0; i < questions.size(); i++) {
+			question = questions.get(i).getAsJsonObject();
+			// System.out.println(question.getAsJsonArray("question"));
+
+			questiondata = question.getAsJsonArray("question");
+
+			for (int j = 0; j < questiondata.size(); j++) {
+				language = questiondata.get(j).getAsJsonObject().get("language").getAsString();
+				if (language.compareTo("en") == 0) {
+					questionstring = questiondata.get(j).getAsJsonObject().get("string").getAsString();
+					query = question.get("query").getAsJsonObject();
+					if (query.isJsonObject() && !query.isJsonNull() && query.has("sparql")) {
+						sparqlquery = question.get("query").getAsJsonObject().get("sparql").getAsString();
+
+						Query myquery = QueryFactory.create(sparqlquery);
+						if (myquery.isSelectType() && myquery.isQueryResultStar()) { // of
+																						// the
+																						// form
+																						// SELECT
+																						// *?
+							myquery.getDatasetDescription(); // FROM / FROM
+																// NAMED
+																// bits
+							Op op = Algebra.compile(myquery); // Get the algebra
+																// for the query
+
+							Element querypattern = myquery.getQueryPattern(); // The
+																				// meat
+																				// of
+																				// the
+																				// query,
+							ElementVisitor v = new ElementVisitor() {
+
+								@Override
+								public void visit(ElementSubQuery el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementService el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementMinus el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementNotExists el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementExists el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementNamedGraph el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementDataset el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementGroup el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementOptional el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementUnion el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementData el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementBind el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementAssign el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementFilter el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementPathBlock el) {
+									// TODO Auto-generated method stub
+
+								}
+
+								@Override
+								public void visit(ElementTriplesBlock el) {
+									// TODO Auto-generated method stub
+									BasicPattern pattern = el.getPattern();
+									pattern.get(0).getMatchSubject().getURI(); // TODO
+
+								}
+							};
+							// the WHERE bit
+							querypattern.visit(v);
+
+						}
+					} else {
+						sparqlquery = null;
+					}
+
+					// TODO: expected output: List of subjects, List of
+					// predicate, List of objects
+
+					System.out
+							.println(question.get("id").getAsInt() + ": " + questionstring + " SPARQL: " + sparqlquery);
+					break;
+				}
+			}
+
+		}
+
+	}
+}
---
+++ qald-evaluator/src/test/java/qald/evaluator/FileReaderTest.java
@@ -0,0 +1,19 @@
+package qald.evaluator;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import eu.wdaqua.qanary.qald.evaluator.qaldreader.FileReader;
+
+public class FileReaderTest {
+
+	@Test
+	public void test() {
+
+		FileReader filereader = new FileReader();
+
+		fail("Not yet implemented");
+	}
+
+}
