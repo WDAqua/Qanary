82d33db392de0b5ea4cf91a64f213d0c5ce44aaf
Andreas Both <andreas.both.de@gmail.com>
2016-10-15 14:19:03 +0200

    re-activate the test

--- src/test/java/qa/pipeline/QanaryQuestionControllerTest.java
+++ src/test/java/qa/pipeline/QanaryQuestionControllerTest.java
@@ -3,9 +3,13 @@ package qa.pipeline;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import org.junit.Test;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import eu.wdaqua.qanary.message.QanaryQuestionCreated;
 import eu.wdaqua.qanary.web.QanaryQuestionController;
@@ -13,24 +17,23 @@ import eu.wdaqua.qanary.web.QanaryQuestionController;
 public class QanaryQuestionControllerTest {
 
 	/**
-	 * test if question string is written correctly
+	 * test if a text question (string) is written correctly
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void testCreateQuestion() throws IOException {
+	public void testCreateTextQuestion() throws IOException {
 
 		QanaryQuestionController qanaryQuestionController = new QanaryQuestionController(null);
 
 		String testquestion = "foo bar?";
 
+		@SuppressWarnings("unchecked")
 		ResponseEntity<QanaryQuestionCreated> response = (ResponseEntity<QanaryQuestionCreated>) qanaryQuestionController
 				.createQuestion(testquestion);
 
-		//String returnedquestion = qanaryQuestionController.getQuestionRawData(response.getBody().getQuestionID());
-
-		//assertTrue(returnedquestion.compareTo(testquestion) == 0);
-
+		FileSystemResource returnedquestion = qanaryQuestionController.getQuestionRawData(response.getBody().getQuestionID());
+		assertTrue(returnedquestion.contentLength() == testquestion.length());
 	}
-
+	
 }
