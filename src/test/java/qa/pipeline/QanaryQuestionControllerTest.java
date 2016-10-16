e7ade3c61f210229f61d793a8732425916457fe5
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-10-16 13:36:17 +0200

    Test added for creation of an audio query

--- src/test/java/qa/pipeline/QanaryQuestionControllerTest.java
+++ src/test/java/qa/pipeline/QanaryQuestionControllerTest.java
@@ -2,12 +2,16 @@ package qa.pipeline;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
@@ -34,6 +38,29 @@ public class QanaryQuestionControllerTest {
 
 		FileSystemResource returnedquestion = qanaryQuestionController.getQuestionRawData(response.getBody().getQuestionID());
 		assertTrue(returnedquestion.contentLength() == testquestion.length());
+	}
+
+	@Test
+	public void testCreateAudioQuestion() throws IOException {
+
+		QanaryQuestionController qanaryQuestionController = new QanaryQuestionController(null);
+
+        //FileSystemResource testquestion = new FileSystemResource("src/test/resources/bill_gates-TED.mp3");
+
+        File file = new File("src/test/resources/bill_gates-TED.mp3");
+        FileInputStream input = new FileInputStream(file);
+        MultipartFile testquestion = new MockMultipartFile("file",
+                file.getName(), "text/plain", IOUtils.toByteArray(input));
+
+
+        //MultipartFile testquestion = new MockMultipartFile("src/test/resources/bill_gates-TED.mp3");
+
+		@SuppressWarnings("unchecked")
+		ResponseEntity<QanaryQuestionCreated> response = (ResponseEntity<QanaryQuestionCreated>) qanaryQuestionController
+				.createAudioQuestion(testquestion);
+
+		FileSystemResource returnedquestion = qanaryQuestionController.getQuestionRawData(response.getBody().getQuestionID());
+		assertTrue(returnedquestion.contentLength() == file.length());
 	}
 	
 }
