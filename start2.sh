8faa469ec53ca5fb660ab29663bf1288ca4e188c
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-11-08 09:42:48 +0100

    Start2 script updated

--- start2.sh
+++ start2.sh
@@ -1,4 +1,4 @@
-PARAMETERS="-Xms256M -Xmx512M -XX:MaxDirectMemorySize=256M -Djava.security.egd=file:/dev/./urandom -Dhttp.proxySet=true -Dhttp.proxyHost=cache.univ-st-etienne.fr -Dhttp.proxyPort=3128 -Dhttp.nonProxyHosts=qanaryhost|qa|wikidatalog"
+PARAMETERS="-Xms256M -Xmx512M -XX:MaxDirectMemorySize=256M -Djava.security.egd=file:/dev/./urandom -Dhttp.proxySet=true -Dhttp.proxyHost=cache.univ-st-etienne.fr -Dhttp.proxyPort=3128 -Dhttp.nonProxyHosts=qanaryhost|qa|wikidatalog|speech"
 killall -9 java
 rm /home/services/stardog-4.1.1/system.lock
 /home/services/stardog-4.1.1/bin/stardog-admin server start
@@ -8,4 +8,5 @@ java $PARAMETERS -jar qanary_component-AGDISTIS-NED/target/qa.AGDISTIS-NED-1.0.0
 java $PARAMETERS -jar qanary_component-DBpedia-Spotlight-NER/target/qa.DBpedia-Spotlight-NER-1.0.0.jar &>> log/spotlight-NER &
 java $PARAMETERS -jar qanary_component-DBpedia-Spotlight-NED/target/qa.DBpedia-Spotlight-NED-1.0.0.jar &>> log/spotlight-NED &
 java $PARAMETERS -jar qanary_component-FOX-NER/target/qa.FOX-NER-1.0.0.jar &>> log/fox-NER &
-java -jar qanary_component-stanford-NER/target/qa.StandfordNER-1.0.0.jar &>> log/stanford-NER &
+java $PARAMETERS -jar qanary_component-stanford-NER/target/qa.StandfordNER-1.0.0.jar &>> log/stanford-NER &
+java $PARAMETERS -jar qanary_component-language-detection/target/qa.language-detection-0.0.1.jar &>> log/language-detection &
