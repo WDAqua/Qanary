fa4bcdb07a4bc1a6333ae7cbedca0f338f6454f5
p-Heinze <46108992+p-Heinze@users.noreply.github.com>
2020-07-27 14:04:50 +0200

    display number of created annotations

--- qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/web/QanaryQuestionAnsweringController.java
+++ qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/web/QanaryQuestionAnsweringController.java
@@ -6,6 +6,12 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.UUID;
 
+import eu.wdaqua.qanary.commons.QanaryUtils;
+import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
+import net.sf.json.JSON;
+import org.apache.jena.atlas.json.JsonArray;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
@@ -15,12 +21,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
@@ -427,6 +428,60 @@ public class QanaryQuestionAnsweringController {
 				myQanaryMessage.getInGraph(), myQanaryMessage.getOutGraph(), qanaryConfigurator);
 
 		return myRun;
+	}
+
+	/**
+	 * get the number of annotations created by a component
+	 */
+	@RequestMapping(value = "/numberOfAnnotations/", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<?> getNumberOfAnnotationsForComponent(
+			@RequestParam String component,
+			@RequestParam String graph
+	) throws URISyntaxException {
+
+		JSONObject json = new JSONObject();
+		URI graphUri = URI.create(graph);
+
+		String sparqlGet ="" +
+				"PREFIX oa: <http://www.w3.org/ns/openannotation/core/>" +
+				"SELECT (COUNT(*) AS ?NumberOfAnnotations)" +
+				"(SAMPLE(STR(?componentname)) AS ?ComponentName)" +
+				"FROM <"+graph+">" +
+				"WHERE {    ?s oa:annotatedBy ?componentname .    " +
+				"FILTER REGEX(STR(?componentname), \""+component+"\") . " +
+				"}";
+
+		QanaryMessage qanaryMessage= new QanaryMessage( qanaryConfigurator.getEndpoint(), graphUri);
+		QanaryUtils qanaryUtils = new QanaryUtils(qanaryMessage);
+
+		try {
+			logger.info("fetching number of annotations with query: {}",sparqlGet);
+
+			ResultSet annotations = qanaryUtils.selectFromTripleStore(sparqlGet, this.qanaryConfigurator.getEndpoint().toString());
+			QuerySolution annotation = annotations.next();
+
+			int annotationCount = 0;
+			String componentUrl = null;
+
+			try {
+				annotationCount = annotation.get("NumberOfAnnotations").asLiteral().getInt();
+				componentUrl = annotation.get("ComponentName").asLiteral().getString();
+				logger.info("found {} annotations for component {} on graph {}",annotationCount,component,graph);
+			} catch (NullPointerException nullPointer) {
+				logger.info("No annotations were found for component {} on graph {}",component,graph);
+			}
+
+			json.put("annotationCount", annotationCount);
+			json.put("componentUrl", componentUrl);
+			json.put("usedGraph", graph);
+			json.put("usedQuery", sparqlGet);
+
+
+		} catch (SparqlQueryFailed queryFailed) {
+			logger.error(queryFailed.getMessage());
+		}
+
+		return new ResponseEntity<>(json, HttpStatus.OK);
 	}
 
 	/**
