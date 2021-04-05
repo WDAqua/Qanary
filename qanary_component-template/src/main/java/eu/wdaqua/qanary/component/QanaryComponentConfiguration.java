16f496b6b760af252ecacf66cd43bf78a451670b
anbo-de <github@andreasboth.de>
2021-04-05 18:36:19 +0200

    made cache beans available in package eu.wdaqua.qanary.component
    now, there is no need to address the qanary commons package directly

--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryComponentConfiguration.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryComponentConfiguration.java
@@ -1,68 +1,105 @@
 package eu.wdaqua.qanary.component;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.cli.MissingArgumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import eu.wdaqua.qanary.commons.config.CacheConfig;
+import eu.wdaqua.qanary.commons.config.RestClientConfig;
+import eu.wdaqua.qanary.communications.CacheOfRestTemplateResponse;
+import eu.wdaqua.qanary.communications.RestTemplateWithCaching;
 
 @Component
+@Configuration
+@EnableCaching
 public class QanaryComponentConfiguration {
-    private final Logger logger = LoggerFactory.getLogger(QanaryComponentConfiguration.class);
-    private Environment environment;
-    private final String[] propertiesToDisplayOnStartup = { //
-            "server.port", //
-            "spring.application.name", //
-            "spring.application.description", //
-            "spring.boot.admin.url", //
-            "spring.boot.admin.client.url"
-    };
-    //add any required parameters
-    private final String[] requiredParameters = {"server.port","spring.application.name","spring.boot.admin.url"};
+	private final Logger logger = LoggerFactory.getLogger(QanaryComponentConfiguration.class);
+	private Environment environment;
+	private final String[] propertiesToDisplayOnStartup = { //
+			"server.port", //
+			"spring.application.name", //
+			"spring.application.description", //
+			"spring.boot.admin.url", //
+			"spring.boot.admin.client.url" };
+	// add any required parameters
+	private final String[] requiredParameters = { "server.port", "spring.application.name", "spring.boot.admin.url" };
 
-    public QanaryComponentConfiguration(@Autowired Environment environment){
-        this.environment = environment;
-    }
+	public QanaryComponentConfiguration(@Autowired Environment environment) {
+		this.environment = environment;
+	}
 
-    @PostConstruct
-    public void validateRequiredArguments() throws MissingArgumentException {
-        for(int i=0;i<requiredParameters.length;i++){
-            if(!this.propertyIsValid(requiredParameters[i])){
-                logger.error("Configuration parameter '" + requiredParameters[i] + "' was not provided.\n\n" //
-                        + "\tRequired: Add parameter '" + requiredParameters[i] + "' to environment configuration\n" //
-                        + "\t(c.f., https://www.tutorialspoint.com/spring_boot/spring_boot_application_properties.htm)\n\n");
-                throw new MissingArgumentException(requiredParameters[i]);
-            }
-        }
-        logger.info("Current Component Configuration: \n{}", this);
-    }
+	/**
+	 * the following four beans are required to make these components in the package
+	 * eu.wdaqua.qanary.component visible
+	 * 
+	 * @return
+	 */
+	@Bean
+	public RestClientConfig myRestClientConfig() {
+		return new RestClientConfig();
+	}
 
-    private boolean propertyIsValid(String property){
-        if (this.propertyExists(property)){
-            if (!this.getPropertyValue(property).equals("")){
-                return true;
-            }
-        }
-        return false;
-    }
+	@Bean
+	public RestTemplateWithCaching myComponentRestClient(RestClientConfig myRestClientConfig)
+			throws NoSuchMethodException, SecurityException {
+		return myRestClientConfig.restTemplateWithCaching(myRestClientConfig.cacheResponse());
+	}
+	
+	@Bean
+	public CacheOfRestTemplateResponse myCacheOfRestTemplateResponse(RestClientConfig myRestClientConfig)
+			throws NoSuchMethodException, SecurityException{
+		return myRestClientConfig.cacheResponse();
+	}
+	
+	@Bean
+	public CacheConfig myCacheConfig() {
+		return new CacheConfig();
+	}
 
-    private boolean propertyExists(String requiredParameter) {
-        return this.environment.containsProperty(requiredParameter);
-    }
+	@PostConstruct
+	public void validateRequiredArguments() throws MissingArgumentException {
+		for (int i = 0; i < requiredParameters.length; i++) {
+			if (!this.propertyIsValid(requiredParameters[i])) {
+				logger.error("Configuration parameter '" + requiredParameters[i] + "' was not provided.\n\n" //
+						+ "\tRequired: Add parameter '" + requiredParameters[i] + "' to environment configuration\n" //
+						+ "\t(c.f., https://www.tutorialspoint.com/spring_boot/spring_boot_application_properties.htm)\n\n");
+				throw new MissingArgumentException(requiredParameters[i]);
+			}
+		}
+		logger.info("Current Component Configuration: \n{}", this);
+	}
 
-    public String getPropertyValue(String property){
-        return this.environment.getProperty(property);
-    }
+	private boolean propertyIsValid(String property) {
+		if (this.propertyExists(property)) {
+			if (!this.getPropertyValue(property).equals("")) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public String toString(){
-        String output = "";
-        for(int i=0;i<propertiesToDisplayOnStartup.length;i++){
-            output += String.format("%-40s = %s\n",
-                    propertiesToDisplayOnStartup[i],this.getPropertyValue(propertiesToDisplayOnStartup[i]));
-        }
-        return output;
-    }
+	private boolean propertyExists(String requiredParameter) {
+		return this.environment.containsProperty(requiredParameter);
+	}
+
+	public String getPropertyValue(String property) {
+		return this.environment.getProperty(property);
+	}
+
+	public String toString() {
+		String output = "";
+		for (int i = 0; i < propertiesToDisplayOnStartup.length; i++) {
+			output += String.format("%-40s = %s\n", propertiesToDisplayOnStartup[i],
+					this.getPropertyValue(propertiesToDisplayOnStartup[i]));
+		}
+		return output;
+	}
 }
