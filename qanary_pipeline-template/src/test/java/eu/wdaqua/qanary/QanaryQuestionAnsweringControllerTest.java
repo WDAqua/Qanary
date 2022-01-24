4b6cf45b0ea7acce790354b3d5ba2b06ff34cad4
heinpa <heipa@posteo.de>
2022-01-24 21:49:50 +0100

    WIP: prior conversation tests for QuestionAnsweringController

--- qanary_pipeline-template/src/test/java/eu/wdaqua/qanary/QanaryQuestionAnsweringControllerTest.java
+++ qanary_pipeline-template/src/test/java/eu/wdaqua/qanary/QanaryQuestionAnsweringControllerTest.java
@@ -1,27 +1,37 @@
 package eu.wdaqua.qanary;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import eu.wdaqua.qanary.commons.QanaryUtils;
 import eu.wdaqua.qanary.web.messages.RequestQuestionAnsweringProcess;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.matches;
+
+import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnectorQanaryInternal;
+
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -35,8 +45,12 @@ class QanaryQuestionAnsweringControllerTest {
 		System.setProperty("stardog.password", "admin");
 	}
 	
-    @Autowired
+	@Autowired
+	@Qualifier("QanaryTripleStoreConnectorQanaryInternal")
     private MockMvc mvc;
+
+	@MockBean
+	private QanaryTripleStoreConnectorQanaryInternal mockedQanaryTripleStoreConnector;
     
     @Test
 	void testRequestQuestionAnsweringProcessToString() throws URISyntaxException {
@@ -62,19 +76,36 @@ class QanaryQuestionAnsweringControllerTest {
 		assertEquals(expected, actual);
 	}
 
+
     @Test
     void testQuestionAnsweringControllerPriorConversation() throws Exception {
         String json = "{\"question\": \"foo?\"}";
-        try (MockedStatic<QanaryUtils> mockedStatic = Mockito.mockStatic(QanaryUtils.class)) {
-            mvc.perform(
-                    MockMvcRequestBuilders.post("/startquestionansweringwithtextquestion")
-                            .contentType("application/json")
-                            .content(json)
-                            .accept(MediaType.APPLICATION_JSON)
-            ).andExpect(status().is2xxSuccessful()); 
 
-            
-            // TODO: assert prior conversation stored by checking the parameter of QanaryTripleStoreConnector.update
-        }
+//		mockedQanaryTripleStoreConnector = 
+//			mock(QanaryTripleStoreConnectorQanaryInternal.class);
+		doNothing().when(mockedQanaryTripleStoreConnector).update(anyString());
+
+		URI priorConversation = new URI("urn:priorConversation");
+		URL questionUri = new URI("http://localhost/question").toURL();
+
+
+		// when post is made to endpoint (and question is created)
+		mvc.perform(
+				MockMvcRequestBuilders.post("/startquestionansweringwithtextquestion")
+					.contentType("application/json")
+					.content(json)
+					.accept(MediaType.APPLICATION_JSON)
+				).andExpect(status().is2xxSuccessful());
+
+
+		// update() 'does nothing' but we can still verify the passed string value
+		// ensure that a SPARQL UPDATE query is called and a graph URI is provided as
+		// prior conversation
+		String queryPart = ".*" //
+				+ "<" + questionUri.toString() + ">" //
+				+ " qa:priorConversation " //
+				+ "<" + priorConversation.toASCIIString() + ">" //
+				+ " \\..*";
+		verify(mockedQanaryTripleStoreConnector, atLeast(1)).update(matches(queryPart));
     }
 }
