67dcc811c02c6df76fd43bb0e2b68a1ae01dab54
anbo-de <github@andreasboth.de>
2023-01-28 17:23:56 +0100

    updated dependencies & test for uncovering runtime
    added tests with uncover runtime errors during compile time

---
+++ qanary_commons/src/test/java/qa/commons/RuntimeDependenciesTest.java
@@ -0,0 +1,21 @@
+package qa.commons;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class RuntimeDependenciesTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		// DatatypeConverter is required, however, it is validated poorly since Java 9
+		// this statement ensures that broken dependencies are uncovered
+		@SuppressWarnings("unused")
+		jakarta.xml.bind.DatatypeConverter justForSafety;
+		javax.xml.bind.annotation.XmlRootElement justForSafety2;
+	}
+
+	@Test
+	void test() {
+	}
+
+}
