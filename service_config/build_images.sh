2db90c3308b112f0c3588ae15d3b3d416a793cf2
caldoe <caldoe@icloud.com>
2024-04-29 12:30:28 +0200

    qa.pipeline 3.8.4
    add ARM support to pipeline

--- service_config/build_images.sh
+++ service_config/build_images.sh
@@ -10,48 +10,8 @@ else
 fi
 
 # build Docker Images and store name and tag
-if ! mvn -B clean install -DskipTests -Dgpg.skip=true;
+if ! mvn -B clean install docker:build docker:push -DskipTests -Dgpg.skip=true;
 then
   # build failed
   exit 1
 fi
-
-echo "Docker images"
-docker image ls
-docker image ls | grep -oP "qanary.*\.[0-9]+ " > images.temp
-
-echo "Locally available Docker images:"
-cat images.temp
-
-# read image list
-images=$(cat images.temp)
-
-i=0
-
-# for each image
-for row in $images
-do
-  # row contains the image name
-  if [ $i -eq 0 ]
-  then
-    # store image name
-    file_name=$row
-    i=$((i + 1))
-  # row contains tag
-  else
-    # generate version and latest tag
-    latest_file_name="${file_name}:latest"
-    file_name="${file_name}:${row}"
-
-    i=0
-
-    # tag images and push to Dockerhub
-    docker tag "${file_name}" "${file_name}"
-    docker tag "${file_name}" "${latest_file_name}"
-    docker push "${file_name}"
-    docker push "${latest_file_name}"
-  fi
-done
-
-# delete temp results
-rm images.temp
