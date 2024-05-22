7b17df420c720775f79589a5ff60d2f4d7b77c7e
Dennis Schiese <schiesedennis@googlemail.com>
2024-05-22 08:21:28 +0200

    Adjusted query Annotation type to LogQuery, removed unused imports

--- qanary_commons/src/main/java/eu/wdaqua/qanary/commons/triplestoreconnectors/QanaryTripleStoreConnectorQanaryInternal.java
+++ qanary_commons/src/main/java/eu/wdaqua/qanary/commons/triplestoreconnectors/QanaryTripleStoreConnectorQanaryInternal.java
@@ -1,116 +1,93 @@
 package eu.wdaqua.qanary.commons.triplestoreconnectors;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.google.api.client.util.Lists;
-import com.stardog.stark.vocabs.RDF;
-import org.apache.http.Header;
-import org.apache.http.HttpHeaders;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
+import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdfconnection.RDFConnection;
-import org.apache.jena.rdfconnection.RDFConnectionRemote;
-import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
-import org.apache.jena.sparql.exec.QueryExecBuilder; // QueryExecBuilder is required for RDFConnection
-import org.apache.jena.update.UpdateFactory;
-import org.apache.jena.update.UpdateRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * simple connection to open endpoint without authorization using the Apache Jena library
- * 
- * @author AnBo
  *
+ * @author AnBo
  */
 public class QanaryTripleStoreConnectorQanaryInternal extends QanaryTripleStoreConnector {
-	private static final Logger logger = LoggerFactory.getLogger(QanaryTripleStoreConnectorQanaryInternal.class);
-	private URI endpoint;
-	private RDFConnection connection;
-	private String applicationName;
-	@Value("${data.logging")
-	private boolean dataLogging;
-	
-	public QanaryTripleStoreConnectorQanaryInternal(URI endpoint, String applicationName) throws URISyntaxException {
-		this.setApplicationName(applicationName);
-		this.endpoint = endpoint;
-		this.connect();
-	}
+    private static final Logger logger = LoggerFactory.getLogger(QanaryTripleStoreConnectorQanaryInternal.class);
+    private URI endpoint;
+    private RDFConnection connection;
+    private String applicationName;
 
-	public String getApplicationName() {
-		return applicationName;
-	}
+    public QanaryTripleStoreConnectorQanaryInternal(URI endpoint, String applicationName) throws URISyntaxException {
+        this.setApplicationName(applicationName);
+        this.endpoint = endpoint;
+        this.connect();
+    }
 
-	private void setApplicationName(String applicationName) {
-		this.applicationName = "urn:qanary:" + applicationName;
-	}
+    public String getApplicationName() {
+        return applicationName;
+    }
 
-	private URI getEndpoint() {
-		return this.endpoint;
-	}
-	
-	@Override
-	public void connect() {
-		org.apache.jena.query.ARQ.init();  // maybe it helps to prevent problems?
-		this.connection = RDFConnection.connect(this.getEndpoint().toASCIIString());
-	}
+    private void setApplicationName(String applicationName) {
+        this.applicationName = "urn:qanary:" + applicationName;
+    }
 
-	@Override
-	public ResultSet select(String sparql) throws SparqlQueryFailed {
-		QueryExecution queryExecution = this.connection.query(sparql);
-		logData(sparql);
-		ResultSet myResultSet = queryExecution.execSelect();
-		ResultSetRewindable resultSet = ResultSetFactory.makeRewindable(myResultSet);
-		return resultSet;
-	}
+    private URI getEndpoint() {
+        return this.endpoint;
+    }
 
-	private void logData(String sparql) {
-		QuerySolutionMap querySolutionMap = new QuerySolutionMap();
-		Query query = QueryFactory.create(sparql);
-		querySolutionMap.add("graph",ResourceFactory.createResource(query.getGraphURIs().get(0)));
-		querySolutionMap.add("component", ResourceFactory.createResource(this.applicationName));
-		querySolutionMap.add("questionID", ResourceFactory.createResource("urn:qanary:currentQuestion"));
-		querySolutionMap.add("body", ResourceFactory.createPlainLiteral(sparql));
-		try {
-			this.update(QanaryTripleStoreConnector.readFileFromResourcesWithMap("/queries/insert_explanation_data_sparql_query.rq", querySolutionMap));
-		} catch(Exception e) {
-			getLogger().error("Logging failed, {}", e);
-		}
+    @Override
+    public void connect() {
+        org.apache.jena.query.ARQ.init();  // maybe it helps to prevent problems?
+        this.connection = RDFConnection.connect(this.getEndpoint().toASCIIString());
+    }
 
-	}
+    @Override
+    public ResultSet select(String sparql) throws SparqlQueryFailed {
+        QueryExecution queryExecution = this.connection.query(sparql);
+        logData(sparql);
+        ResultSet myResultSet = queryExecution.execSelect();
+        ResultSetRewindable resultSet = ResultSetFactory.makeRewindable(myResultSet);
+        return resultSet;
+    }
 
-	@Override
-	public void update(String sparql, URI graph) throws SparqlQueryFailed {
-		this.update(sparql);
-	}
+    private void logData(String sparql) {
+        QuerySolutionMap querySolutionMap = new QuerySolutionMap();
+        Query query = QueryFactory.create(sparql);
+        querySolutionMap.add("graph", ResourceFactory.createResource(query.getGraphURIs().get(0)));
+        querySolutionMap.add("component", ResourceFactory.createResource(this.applicationName));
+        querySolutionMap.add("questionID", ResourceFactory.createResource("urn:qanary:currentQuestion"));
+        querySolutionMap.add("body", ResourceFactory.createPlainLiteral(sparql));
+        try {
+            this.update(QanaryTripleStoreConnector.readFileFromResourcesWithMap("/queries/insert_explanation_data_sparql_query.rq", querySolutionMap));
+        } catch (Exception e) {
+            getLogger().error("Logging failed, {}", e);
+        }
 
-	@Override
-	public void update(String sparql) throws SparqlQueryFailed {
-		logger.debug("UPDATE on {}: {}", this.getEndpoint().toASCIIString(), sparql);
-		this.connection.update(sparql);
-	}
+    }
 
-	@Override
-	public boolean ask(String sparql) throws SparqlQueryFailed {
-		logData(sparql);
-		return this.connection.queryAsk(sparql);
-	}
+    @Override
+    public void update(String sparql, URI graph) throws SparqlQueryFailed {
+        this.update(sparql);
+    }
 
-	@Override
-	public String getFullEndpointDescription() {
-		return "simple open endpoint without authorization: " + this.getEndpoint().toASCIIString();
-	}
+    @Override
+    public void update(String sparql) throws SparqlQueryFailed {
+        logger.debug("UPDATE on {}: {}", this.getEndpoint().toASCIIString(), sparql);
+        this.connection.update(sparql);
+    }
+
+    @Override
+    public boolean ask(String sparql) throws SparqlQueryFailed {
+        logData(sparql);
+        return this.connection.queryAsk(sparql);
+    }
+
+    @Override
+    public String getFullEndpointDescription() {
+        return "simple open endpoint without authorization: " + this.getEndpoint().toASCIIString();
+    }
 }
