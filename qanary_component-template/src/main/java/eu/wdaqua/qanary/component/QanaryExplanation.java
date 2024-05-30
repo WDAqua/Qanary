a46f78a91f6d2f0499d921b4aa887afd71e5865c
Dennis Schiese <schiesedennis@googlemail.com>
2024-05-30 13:44:43 +0200

    Create Explanation service with placeholder values

---
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanation.java
@@ -0,0 +1,18 @@
+package eu.wdaqua.qanary.component;
+
+public class QanaryExplanation {
+
+    public void explain() {
+        ///// What to take into account?
+
+        // explanation of each child
+            // ...
+
+        // Question - only pipeline or *real* components too?
+            // ...
+
+        // Java <-> RDF
+            // ...
+    }
+
+}
