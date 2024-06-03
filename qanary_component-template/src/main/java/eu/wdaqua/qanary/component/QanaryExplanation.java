a4b69ff4c8f230bec152e479ccca9de8fe8616e0
Dennis Schiese <schiesedennis@googlemail.com>
2024-06-03 11:18:24 +0200

    Added component selection query and extended explanation process

--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanation.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanation.java
@@ -1,18 +1,36 @@
 package eu.wdaqua.qanary.component;
 
+import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
+import org.apache.jena.query.QuerySolutionMap;
+import org.apache.jena.rdf.model.ResourceFactory;
+
+import java.io.IOException;
+import java.util.List;
+
 public class QanaryExplanation {
 
-    public void explain() {
+    private static final String SELECT_ALL_COMPONENTS_QUERY = "/queries/select_all_used_components.rq";
+
+    // Good caching candidate
+    // Explanations aren't stored in the triplestore, instead, the /explain endpoint is used
+    public void explain(String graphURI) {
         ///// What to take into account?
 
         // explanation of each child
-            // ...
+        List<String> usedComponents;
 
         // Question - only pipeline or *real* components too?
             // ...
 
         // Java <-> RDF
             // ...
+    }
+
+    public void selectAllComponents(String graphURI) throws IOException {
+        QuerySolutionMap querySolutionMap = new QuerySolutionMap();
+        querySolutionMap.add("graph", ResourceFactory.createResource(graphURI));
+        String query = QanaryTripleStoreConnector.readFileFromResourcesWithMap(SELECT_ALL_COMPONENTS_QUERY,querySolutionMap);
+        // return components, select query
     }
 
 }
