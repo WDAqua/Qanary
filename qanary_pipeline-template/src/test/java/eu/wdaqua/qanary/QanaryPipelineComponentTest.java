8749f91a4f7bca8cc9b411728df0c2c0e37b8ce7
Dennis Schiese <schiesedennis@googlemail.com>
2024-06-28 11:22:22 +0200

    Fixed tests due to QanaryTripleStoreProxy, Added new test for QanaryPipelineCompoennt

--- qanary_pipeline-template/src/test/java/eu/wdaqua/qanary/QanaryPipelineComponentTest.java
+++ qanary_pipeline-template/src/test/java/eu/wdaqua/qanary/QanaryPipelineComponentTest.java
@@ -1,49 +1,63 @@
 package eu.wdaqua.qanary;
 
-import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
-import org.apache.http.HttpResponse;
+import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnectorVirtuoso;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.util.Objects;
+
+import static org.mockito.Mockito.doNothing;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureWireMock(port = 8080) //
 public class QanaryPipelineComponentTest {
 
-    private final String questionID = "http://localhost:8080";
-    private final String EXAMPLE_QUESTION = "Example question";
-
+    protected ClassLoader classLoader = this.getClass().getClassLoader();
     @MockBean
-    private QanaryTripleStoreConnector qanaryTripleStoreConnector;
+    QanaryTripleStoreConnectorVirtuoso qanaryTripleStoreConnectorVirtuoso;
+    @InjectMocks
+    QanaryPipelineComponent qanaryPipelineComponent;
+    URI testUri = new URI("testUri");
 
-    @Autowired
-    private QanaryPipelineComponent qanaryPipelineComponent;
+    public QanaryPipelineComponentTest() throws URISyntaxException {
+    }
 
     @Before
     public void setup() {
-        //doNothing().when(qanaryTripleStoreConnector).connect();
-        WebClient webClient = WebClient.builder().build();
-        ReflectionTestUtils.setField(qanaryPipelineComponent, "webClient", webClient);
+        doNothing().when(qanaryTripleStoreConnectorVirtuoso).connect();
     }
 
-    private String convertResponseToString(HttpResponse response) throws IOException {
-        InputStream responseStream = response.getEntity().getContent();
-        Scanner scanner = new Scanner(responseStream, "UTF-8");
-        String responseString = scanner.useDelimiter("\\Z").next();
-        scanner.close();
-        return responseString;
-    }
+    @Test
+    public void constructQueryFromModelTest() throws IOException {
+        Model model = ModelFactory.createDefaultModel();
+        model.add(ResourceFactory.createStatement(
+                ResourceFactory.createResource("annotation"),
+                ResourceFactory.createProperty("oa:annotatedBy"),
+                ResourceFactory.createPlainLiteral("test")
+        ));
+        String query = qanaryPipelineComponent.constructQueryFromModel(model, testUri, "/insert_constructed_triples.rq");
+        String expectedResult = readFileFromTestResources("constructQueryFromModelTestResultQuery");
 
+        Assertions.assertNotEquals(expectedResult, query);
+    }
+    
+    private String readFileFromTestResources(String path) throws IOException {
+        File file = new File(Objects.requireNonNull(classLoader.getResource(path)).getFile());
+        return new String(Files.readAllBytes(file.toPath()));
+    }
 
 }
--- qanary_pipeline-template/src/test/java/eu/wdaqua/qanary/QanaryQuestionAnsweringControllerTest.java
+++ qanary_pipeline-template/src/test/java/eu/wdaqua/qanary/QanaryQuestionAnsweringControllerTest.java
@@ -1,21 +1,8 @@
 package eu.wdaqua.qanary;
 
-import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.matches;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.io.FileNotFoundException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-
+import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreProxy;
+import eu.wdaqua.qanary.web.QanaryStandardWebParameters;
+import eu.wdaqua.qanary.web.messages.RequestQuestionAnsweringProcess;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
@@ -34,9 +21,18 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.util.NestedServletException;
 
-import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
-import eu.wdaqua.qanary.web.QanaryStandardWebParameters;
-import eu.wdaqua.qanary.web.messages.RequestQuestionAnsweringProcess;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest(classes = QanaryPipeline.class, webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -44,33 +40,31 @@ import eu.wdaqua.qanary.web.messages.RequestQuestionAnsweringProcess;
 @ContextConfiguration(name = "contextWithFakeBean")
 class QanaryQuestionAnsweringControllerTest {
 
+    static {
+        System.setProperty("springdoc.api-docs.path", "/api-docs");
+        System.setProperty("qanary.process.allow-additional-triples", "true");
+    }
+
     private final Logger logger = LoggerFactory.getLogger(QanaryQuestionAnsweringControllerTest.class);
-	
-	@Autowired
+    @Autowired
     private MockMvc mvc;
+    @MockBean
+    private QanaryTripleStoreProxy mockedQanaryTripleStoreConnector;
 
-	@MockBean
-	private QanaryTripleStoreConnector mockedQanaryTripleStoreConnector;
-
-	static {
-		System.setProperty("springdoc.api-docs.path", "/api-docs");
-		System.setProperty("qanary.process.allow-additional-triples", "true");
-  	}
-	
     @Test
     void testRequestQuestionAnsweringProcessToStringWithoutLanguage() throws URISyntaxException {
         RequestQuestionAnsweringProcess qaProcess = new RequestQuestionAnsweringProcess();
 
-		// given a text question, two components and an existing priorConversation
-		String question = "What is the real name of Batman";
-		List<String> componentList = new ArrayList<>();
-		componentList.add("NED-DBpediaSpotlight");
-		componentList.add("QueryBuilderSimpleRealNameOfSuperHero");
-		URI priorConversation = new URI("urn:graph:806261d9-4601-4c8c-8603-926eee707c38");
+        // given a text question, two components and an existing priorConversation
+        String question = "What is the real name of Batman";
+        List<String> componentList = new ArrayList<>();
+        componentList.add("NED-DBpediaSpotlight");
+        componentList.add("QueryBuilderSimpleRealNameOfSuperHero");
+        URI priorConversation = new URI("urn:graph:806261d9-4601-4c8c-8603-926eee707c38");
 
-		qaProcess.setQuestion(question);
-		qaProcess.setcomponentlist(componentList);
-		qaProcess.setPriorConversation(priorConversation);
+        qaProcess.setQuestion(question);
+        qaProcess.setcomponentlist(componentList);
+        qaProcess.setPriorConversation(priorConversation);
 
         String expected = "RequestQuestionAnsweringProcess " + // 
                 " -- question: \"What is the real name of Batman\"" + // 
@@ -105,191 +99,195 @@ class QanaryQuestionAnsweringControllerTest {
                 " -- language: es";
 
         String actual = qaProcess.toString();
-        
-		assertEquals(expected, actual, "\nexpected:\n" + expected + " \n != \nactual:\n" + actual );
-	}
+
+        assertEquals(expected, actual, "\nexpected:\n" + expected + " \n != \nactual:\n" + actual);
+    }
 
 
     @Test
     void testQuestionAnsweringControllerPriorConversation() throws Exception {
 
-		// mock update() of QanaryTripleStoreConnector to remove its internal logic 
-		doNothing().when(mockedQanaryTripleStoreConnector).update(anyString()); 
+        // mock update() of QanaryTripleStoreConnector to remove its internal logic
+        doNothing().when(mockedQanaryTripleStoreConnector).update(anyString());
 
-		// given question with reference to a prior conversation
-		URI priorConversation = new URI("urn:priorConversation");
-		String json = "{\""+QanaryStandardWebParameters.QUESTION+"\": \"foo?\", \""+QanaryStandardWebParameters.PRIORCONVERSATION+"\": \""+ priorConversation.toASCIIString() + "\"}"; 
+        // given question with reference to a prior conversation
+        URI priorConversation = new URI("urn:priorConversation");
+        String json = "{\"" + QanaryStandardWebParameters.QUESTION + "\": \"foo?\", \"" + QanaryStandardWebParameters.PRIORCONVERSATION + "\": \"" + priorConversation.toASCIIString() + "\"}";
 
-		// when a successful post request is made to /startquestionansweringwithtextquestion
-		// to start the question answering process 
-		mvc.perform(
-				MockMvcRequestBuilders.post("/startquestionansweringwithtextquestion")
-					.contentType("application/json")
-					.content(json)
-					.accept(MediaType.APPLICATION_JSON)
-				).andExpect(status().is2xxSuccessful());
+        // when a successful post request is made to /startquestionansweringwithtextquestion
+        // to start the question answering process
+        mvc.perform(
+                MockMvcRequestBuilders.post("/startquestionansweringwithtextquestion")
+                        .contentType("application/json")
+                        .content(json)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is2xxSuccessful());
 
-		// then the SPARQL UPDATE query is called and a graph URI is provided as 
-		// prior conversation 
-		String queryPart = ".*" //
-				+ " qa:priorConversation " //
-				+ "<" + priorConversation.toASCIIString() + ">" //
-				+ " \\..*";
+        // then the SPARQL UPDATE query is called and a graph URI is provided as
+        // prior conversation
+        String queryPart = ".*" //
+                + " qa:priorConversation " //
+                + "<" + priorConversation.toASCIIString() + ">" //
+                + " \\..*";
 
- 		// update() 'does nothing' but we can still verify the passed string value
- 		// is it called at least once?
-		verify(mockedQanaryTripleStoreConnector, atLeast(1)).update(anyString());
+        // update() 'does nothing' but we can still verify the passed string value
+        // is it called at least once?
+        verify(mockedQanaryTripleStoreConnector, atLeast(1)).update(anyString());
 
- 		// is is called at least once with the expected parameter value?
-		verify(mockedQanaryTripleStoreConnector, atLeast(1)).update(matches(queryPart));
+        // is is called at least once with the expected parameter value?
+        verify(mockedQanaryTripleStoreConnector, atLeast(1)).update(matches(queryPart));
     }
-    
+
     /**
      * IMPORTANT: for some reason this test does not work in the GitHub action
-     * 
+     * <p>
      * test availability of api-docs via Web service (required to ensure OpenAPI/Swagger docs availability)
-     * 
-     * problem might be discovered when springdoc-openapi package has a conflict (e.g., v1.6.14) 
-     * 
+     * <p>
+     * problem might be discovered when springdoc-openapi package has a conflict (e.g., v1.6.14)
+     *
      * @throws Exception
      */
     @Test
     void testOpenApiDefinitionAvailability() throws Exception {
-    	String openApiPath = System.getProperty("springdoc.api-docs.path") + ".yaml";
-    	logger.info("test availability of OpenAPI YAML file at {}", openApiPath);
-    	MvcResult x = mvc.perform(MockMvcRequestBuilders.get(openApiPath)).andReturn();
-    	int httpCode = x.getResponse().getStatus();
-    	if( httpCode >= 200 && httpCode < 400 ) {
-    		logger.info("Access to {} results in HTTP {}.", openApiPath, httpCode);
-    	} else {
-    		logger.error("Access to {} results in HTTP {}. Check springdoc-openapi!", openApiPath, httpCode);
-    	}
+        String openApiPath = System.getProperty("springdoc.api-docs.path") + ".yaml";
+        logger.info("test availability of OpenAPI YAML file at {}", openApiPath);
+        MvcResult x = mvc.perform(MockMvcRequestBuilders.get(openApiPath)).andReturn();
+        int httpCode = x.getResponse().getStatus();
+        if (httpCode >= 200 && httpCode < 400) {
+            logger.info("Access to {} results in HTTP {}.", openApiPath, httpCode);
+        } else {
+            logger.error("Access to {} results in HTTP {}. Check springdoc-openapi!", openApiPath, httpCode);
+        }
     }
 
     @Test
     void testWithoutAdditionalTriples() throws Exception {
-    	String url = "/startquestionansweringwithtextquestion";
+        String url = "/startquestionansweringwithtextquestion";
 
-    	JSONArray componentList = new org.json.JSONArray();
-    	componentList.put("OpenTapiocaNED");
-    	componentList.put("BirthDataQueryBuilderWikidata");
-    	
-    	JSONObject json = new JSONObject();
-    	json.put("question", "When was Albert Einstein born?");
-    	json.put("componentlist", componentList);
-    	json.put("language", "en");
-    	
-    	mvc.perform(MockMvcRequestBuilders.post(url) // 
-				.contentType("application/json")
-				.content(json.toString())
-				.with(csrf())
-				.accept(MediaType.APPLICATION_JSON)
-		).andExpect(status().is2xxSuccessful());;
-    	
+        JSONArray componentList = new org.json.JSONArray();
+        componentList.put("OpenTapiocaNED");
+        componentList.put("BirthDataQueryBuilderWikidata");
+
+        JSONObject json = new JSONObject();
+        json.put("question", "When was Albert Einstein born?");
+        json.put("componentlist", componentList);
+        json.put("language", "en");
+
+        mvc.perform(MockMvcRequestBuilders.post(url) //
+                .contentType("application/json")
+                .content(json.toString())
+                .with(csrf())
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is2xxSuccessful());
+        ;
+
     }
 
     @Test
     void testWithEmptyAdditionalTriples() throws Exception {
-    	String url = "/startquestionansweringwithtextquestion";
+        String url = "/startquestionansweringwithtextquestion";
 
-    	JSONArray componentList = new org.json.JSONArray();
-    	componentList.put("OpenTapiocaNED");
-    	componentList.put("BirthDataQueryBuilderWikidata");
-    	
-    	JSONObject json = new JSONObject();
-    	json.put("question", "When was Albert Einstein born?");
-    	json.put("componentlist", componentList);
-    	json.put("language", "en");
-    	json.put("additionalTriples", "");
-    	
-    	mvc.perform(MockMvcRequestBuilders.post(url) // 
-				.contentType("application/json")
-				.content(json.toString())
-				.with(csrf())
-				.accept(MediaType.APPLICATION_JSON)
-		).andExpect(status().is2xxSuccessful());;
-    	
+        JSONArray componentList = new org.json.JSONArray();
+        componentList.put("OpenTapiocaNED");
+        componentList.put("BirthDataQueryBuilderWikidata");
+
+        JSONObject json = new JSONObject();
+        json.put("question", "When was Albert Einstein born?");
+        json.put("componentlist", componentList);
+        json.put("language", "en");
+        json.put("additionalTriples", "");
+
+        mvc.perform(MockMvcRequestBuilders.post(url) //
+                .contentType("application/json")
+                .content(json.toString())
+                .with(csrf())
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is2xxSuccessful());
+        ;
+
     }
-  
+
     @Test
     void testWithAdditionalTriple() throws Exception {
-    	String url = "/startquestionansweringwithtextquestion";
+        String url = "/startquestionansweringwithtextquestion";
 
-    	JSONArray componentList = new org.json.JSONArray();
-    	componentList.put("OpenTapiocaNED");
-    	componentList.put("BirthDataQueryBuilderWikidata");
-    	
-    	JSONObject json = new JSONObject();
-    	json.put("question", "When was Albert Einstein born?");
-    	json.put("componentlist", componentList);
-    	json.put("language", "en");
-    	json.put("additionalTriples", "<urn:s> <urn:p> <urn:o>  .");
-    	
-    	mvc.perform(MockMvcRequestBuilders.post(url) // 
-				.contentType("application/json")
-				.content(json.toString())
-				.with(csrf())
-				.accept(MediaType.APPLICATION_JSON)
-		).andExpect(status().is2xxSuccessful());;
-    	
+        JSONArray componentList = new org.json.JSONArray();
+        componentList.put("OpenTapiocaNED");
+        componentList.put("BirthDataQueryBuilderWikidata");
+
+        JSONObject json = new JSONObject();
+        json.put("question", "When was Albert Einstein born?");
+        json.put("componentlist", componentList);
+        json.put("language", "en");
+        json.put("additionalTriples", "<urn:s> <urn:p> <urn:o>  .");
+
+        mvc.perform(MockMvcRequestBuilders.post(url) //
+                .contentType("application/json")
+                .content(json.toString())
+                .with(csrf())
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is2xxSuccessful());
+        ;
+
     }
 
     @Test
     void testWithAdditionalTriples() throws Exception {
-    	String url = "/startquestionansweringwithtextquestion";
+        String url = "/startquestionansweringwithtextquestion";
 
-    	JSONArray componentList = new org.json.JSONArray();
-    	componentList.put("OpenTapiocaNED");
-    	componentList.put("BirthDataQueryBuilderWikidata");
-    	
-    	JSONObject json = new JSONObject();
-    	json.put("question", "When was Albert Einstein born?");
-    	json.put("componentlist", componentList);
-    	json.put("language", "en");
-		json.put("additionalTriples", "" //
-				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " // 
-				+ "PREFIX xyz: <urn:xyz:> " //
-				+ "<urn:s0> xyz:p0 <urn:o0> ." //
-				+ "<urn:s1>   <urn:p1> <urn:o1> .     " //
-				+ "<urn:s2> <urn:p2> \"1\"^^xsd:integer    . " //
-				+ "<urn:s3> <urn:p3>    \"string\" . " //
-				+ "<urn:s4> <urn:p4> \"explicit string\"^^xsd:string . " //
-				+ "<urn:s5>   <urn:p5> <urn:o5>,\"string\" . " //
-				+ "");    	
-    	
-    	mvc.perform(MockMvcRequestBuilders.post(url) // 
-				.contentType("application/json")
-				.content(json.toString())
-				.with(csrf())
-				.accept(MediaType.APPLICATION_JSON)
-		).andExpect(status().is2xxSuccessful());;
-    	
+        JSONArray componentList = new org.json.JSONArray();
+        componentList.put("OpenTapiocaNED");
+        componentList.put("BirthDataQueryBuilderWikidata");
+
+        JSONObject json = new JSONObject();
+        json.put("question", "When was Albert Einstein born?");
+        json.put("componentlist", componentList);
+        json.put("language", "en");
+        json.put("additionalTriples", "" //
+                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " //
+                + "PREFIX xyz: <urn:xyz:> " //
+                + "<urn:s0> xyz:p0 <urn:o0> ." //
+                + "<urn:s1>   <urn:p1> <urn:o1> .     " //
+                + "<urn:s2> <urn:p2> \"1\"^^xsd:integer    . " //
+                + "<urn:s3> <urn:p3>    \"string\" . " //
+                + "<urn:s4> <urn:p4> \"explicit string\"^^xsd:string . " //
+                + "<urn:s5>   <urn:p5> <urn:o5>,\"string\" . " //
+                + "");
+
+        mvc.perform(MockMvcRequestBuilders.post(url) //
+                .contentType("application/json")
+                .content(json.toString())
+                .with(csrf())
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is2xxSuccessful());
+        ;
+
     }
 
     @Test
     void testWithBrokenAdditionalTriplesShouldFail() throws Exception {
-    	String url = "/startquestionansweringwithtextquestion";
+        String url = "/startquestionansweringwithtextquestion";
 
-    	JSONArray componentList = new org.json.JSONArray();
-    	componentList.put("OpenTapiocaNED");
-    	componentList.put("BirthDataQueryBuilderWikidata");
-    	
-    	JSONObject json = new JSONObject();
-    	json.put("question", "When was Albert Einstein born?");
-    	json.put("componentlist", componentList);
-    	json.put("language", "en");
-    	json.put("additionalTriples", "<urn:s <urn:p> <urn:o>  .");
-    	
-    	Assertions.assertThrows(NestedServletException.class, () -> {
-	    	mvc.perform(MockMvcRequestBuilders.post(url) // 
-					.contentType("application/json")
-					.content(json.toString())
-					.with(csrf())
-					.accept(MediaType.APPLICATION_JSON)
-			).andReturn();
-	    	fail("Request should have failed");
-   	   });
-    	
+        JSONArray componentList = new org.json.JSONArray();
+        componentList.put("OpenTapiocaNED");
+        componentList.put("BirthDataQueryBuilderWikidata");
+
+        JSONObject json = new JSONObject();
+        json.put("question", "When was Albert Einstein born?");
+        json.put("componentlist", componentList);
+        json.put("language", "en");
+        json.put("additionalTriples", "<urn:s <urn:p> <urn:o>  .");
+
+        Assertions.assertThrows(NestedServletException.class, () -> {
+            mvc.perform(MockMvcRequestBuilders.post(url) //
+                    .contentType("application/json")
+                    .content(json.toString())
+                    .with(csrf())
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andReturn();
+            fail("Request should have failed");
+        });
+
     }
 
 }
