41f2c8a7716244fb16c92a5eb7abf738d618a8c0
Dennis Schiese <schiesedennis@googlemail.com>
2024-07-23 10:27:29 +0200

    WIP

--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanation.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanation.java
@@ -1,36 +1,48 @@
 package eu.wdaqua.qanary.component;
 
 import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
+import eu.wdaqua.qanary.message.QanaryQuestionAnsweringRun;
 import org.apache.jena.query.QuerySolutionMap;
 import org.apache.jena.rdf.model.ResourceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.util.List;
 
 public class QanaryExplanation {
 
-    private static final String SELECT_ALL_COMPONENTS_QUERY = "/queries/select_all_used_components.rq";
+
+    private final String GRAPH_QUERY = "PREFIX oa: <http://www.w3.org/ns/openannotation/core/>\n" +
+            "PREFIX qa: <http://www.wdaqua.eu/qa#>\n" +
+            "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+            "\n" +
+            "SELECT DISTINCT ?graph ?p ?o\n" +
+            "WHERE {\n" +
+            "\tGRAPH ?graph {\n" +
+            "  \t\t?questionId owl:sameAs <urn:qanary:currentQuestion> .\n" +
+            "  \t}\n" +
+            "}";
 
     // Good caching candidate
     // Explanations aren't stored in the triplestore, instead, the /explain endpoint is used
-    public void explain(String graphURI) {
-        ///// What to take into account?
+    public void explain(QanaryQuestionAnsweringRun qanaryQuestionAnsweringRun) {
+        // PaC can have two endpoints
 
-        // explanation of each child
-        List<String> usedComponents;
-
-        // Question - only pipeline or *real* components too?
-            // ...
-
-        // Java <-> RDF
-            // ...
     }
 
-    public void selectAllComponents(String graphURI) throws IOException {
-        QuerySolutionMap querySolutionMap = new QuerySolutionMap();
-        querySolutionMap.add("graph", ResourceFactory.createResource(graphURI));
-        String query = QanaryTripleStoreConnector.readFileFromResourcesWithMap(SELECT_ALL_COMPONENTS_QUERY,querySolutionMap);
-        // return components, select query
+    /**
+     * Get all used components by filtering those which created AnnotationOfLog+ annotations
+     * @throws IOException
+     */
+    public void getAllUsedComponents(String graph) throws IOException {
+
+    }
+
+    public void getAllGraphs(String questionId) {
+        QuerySolutionMap qsm = new QuerySolutionMap();
+        qsm.add("questionId", ResourceFactory.createResource(questionId));
+        String query = GRAPH_QUERY.replace("?questionId", "<" + questionId + ">");
+
     }
 
 }
--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanationController.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanationController.java
@@ -1,18 +1,24 @@
 package eu.wdaqua.qanary.component;
 
+import eu.wdaqua.qanary.message.QanaryQuestionAnsweringRun;
 import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class QanaryExplanationController {
 
     private static final Logger logger = LoggerFactory.getLogger(QanaryExplanationController.class);
+
+    @Autowired
+    QanaryExplanation qanaryExplanationService;
 
     @GetMapping(value = {"/explain/{graphUri}"})
     @Operation(
@@ -20,7 +26,7 @@ public class QanaryExplanationController {
             operationId = "explainComponent", //
             description = "Returns an explanation for this component within the specified graph." //
     )
-    public ResponseEntity<?> explainComponent(@PathVariable String graphUri) { // To be done
+    public ResponseEntity<?> explainComponent(@RequestBody QanaryQuestionAnsweringRun qanaryQuestionAnsweringRun) { // To be done
         return new ResponseEntity<>("Not yet implemented", HttpStatus.BAD_REQUEST);
     }
 
