a35c7ce83ad3baead8549c21032bf134e34be0ba
Dennis Schiese <schiesedennis@googlemail.com>
2024-07-24 11:24:41 +0200

    [Done]: Implementation, [WIP]: Tests

--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanation.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanation.java
@@ -1,5 +1,6 @@
 package eu.wdaqua.qanary.component;
 
+import com.complexible.stardog.plan.filter.functions.numeric.E;
 import eu.wdaqua.qanary.business.QanaryConfigurator;
 import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreConnector;
 import eu.wdaqua.qanary.commons.triplestoreconnectors.QanaryTripleStoreProxy;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 import java.net.URI;
@@ -29,98 +31,55 @@ public class QanaryExplanation {
     @Autowired
     private QanaryTripleStoreConnector qanaryTripleStoreConnector;
     private final String GRAPH_QUERY = "/queries/select_all_graphs_with_questionId.rq";
-    private final String SELECT_ALL_USED_COMPONENTS_QUERY = "/queries/select_all_used_components.rq";
+    private final String ANNOTATED_BY_COMPONENT_TRIPLE = "?s oa:annotatedBy ?component .";
 
+    @Value("${explanation.service.uri}")
+    private String EXPLANATION_SERVICE_URI;
     @Value("${spring.application.name}")
     private String applicationName;
-
-    @Value("${pipeline.as.component}")
-    private String isPipeline;
-
-    private QanaryConfigurator qanaryConfigurator;
-    private URI endpoint;
+    @Value("${component.list}") // TODO: What, if component list not available? If it's a component? -> Is it null, empty or does it throw an error?
+    private ArrayList<String> componentList;
 
     @Autowired
-    public void setQanaryConfigurator(QanaryConfigurator qanaryConfigurator) {
-        this.endpoint = qanaryConfigurator.getEndpoint();
+    WebClient webClient;
+
+    public QanaryExplanation() {
+        webClient = WebClient.builder().build();
     }
 
-    @ConditionalOnProperty
+    public String explain(String graph, String questionId) throws URISyntaxException, SparqlQueryFailed, IOException {
 
-    // Good caching candidate
-    // Explanations aren't stored in the triplestore, instead, the /explain endpoint is used
-    public String explain(QanaryExplanationDTO qanaryExplanationDTO) throws URISyntaxException, SparqlQueryFailed, IOException {
-        if(qanaryExplanationDTO.getRootGraph() != null && qanaryExplanationDTO.getPriorGraph() == null) {
-            List<String> components = getAllUsedComponents(qanaryExplanationDTO.getRootGraph());
-            components.forEach(component -> {
-
-            });
+        if(componentList == null) { // Case it's a component
+            return webClient.get().uri(new URI(EXPLANATION_SERVICE_URI + "/explain/" + graph + "/" + this.applicationName)).retrieve().bodyToMono(String.class).block();
         }
-        else {
-            if(isPipeline == "true") { // Component is PaC oder Root pipeline
-                boolean doesEndpointExist = qanaryExplanationDTO.doesEndpointExist(endpoint);
-                URI graph = doesEndpointExist ?
-                        qanaryExplanationDTO.popAndReturnGraph(endpoint)
-                        :
-                        qanaryExplanationDTO.setNewEndpointWithGraph(endpoint, this.getAllGraphsFromQuestionId(qanaryExplanationDTO.getQuestionId()));
-
+        else { // Case it's not an atomic component
+            if(componentList.isEmpty()) { // Case it's the pipeline, the direct child-components can be fetched by the expl. service with the graph
+                return webClient.get().uri(new URI(EXPLANATION_SERVICE_URI + "/explain/" + graph)).retrieve().bodyToMono(String.class).block();
             }
-            else {
-                // Is a concrete component, request explanation
+            else { // Case it's a pipeline as component, the graph needs to be selected, then proceed like a pipeline
+                String currentGraph = getGraphFromQuestionId(questionId);
+                return webClient.get().uri(new URI(EXPLANATION_SERVICE_URI + "/explain/" + currentGraph)).retrieve().bodyToMono(String.class).block();
             }
-
-
         }
     }
 
-    public List<String> getAllUsedComponents(URI graph) throws IOException, URISyntaxException, SparqlQueryFailed {
-        List<String> components = new ArrayList<>();
-        QuerySolutionMap qsm = new QuerySolutionMap();
-        qsm.add("graph", ResourceFactory.createResource(graph.toASCIIString()));
-        String query = QanaryTripleStoreConnector.readFileFromResourcesWithMap(SELECT_ALL_USED_COMPONENTS_QUERY, qsm);
-        ResultSet results = qanaryTripleStoreConnector.select(query);
-        while(results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-            components.add(solution.get("component").toString());
-        }
-        return components;
+    public String getGraphFromQuestionId(String questionId) throws IOException, URISyntaxException, SparqlQueryFailed {
+        String componentTriples = createComponentTriples();
+        String query = QanaryTripleStoreConnector.readFileFromResources(GRAPH_QUERY)
+                .replace("?componentAnnotations", componentTriples)
+                .replace("?questionId", "<" + questionId + ">");
+        // Ignore problem of multiple graphs at a time; annotatedAt could partly solve this
+        return qanaryTripleStoreConnector.select(query).next().get("g").toString();
     }
 
-    // TODO: Sort graphs
-    public ArrayList<URI> getAllGraphsFromQuestionId(URI questionId) throws URISyntaxException, SparqlQueryFailed {
-        ArrayList<URI> graphs = new ArrayList<>();
-        QuerySolutionMap qsm = new QuerySolutionMap();
-        qsm.add("questionId", ResourceFactory.createResource(questionId.toASCIIString()));
-        String query = GRAPH_QUERY.replace("?questionId", "<" + questionId + ">");
-        ResultSet results = qanaryTripleStoreConnector.select(query);
-        while(results.hasNext()) {
-            QuerySolution solution = results.nextSolution();
-            graphs.add(new URI(solution.get("graph").toString()));
+    public String createComponentTriples() {
+        String triples = "";
+        for (String s : componentList) {
+            triples += ANNOTATED_BY_COMPONENT_TRIPLE
+                    .replace("?s", "?" + s)
+                    .replace("?component", "urn:qanary:" + s);
         }
-        return graphs;
+        return triples;
     }
 
 }
-
-
-/*
-        List<String> usedComponents = getAllUsedComponents(graph); // These are the direct child's
-
-
-        List<String> possibleGraphs = getAllGraphs(questionId);
-        if(possibleGraphs.size() == 0)
-            return null;
-        else if (possibleGraphs.size() == 1) {
-
-        }
-        else { // We've to map the graphs to the components
-            Map<String,String> graphAndComponent = new HashMap<>();
-        }
-
-        return null;
-
- --------
-
-
-
- */
