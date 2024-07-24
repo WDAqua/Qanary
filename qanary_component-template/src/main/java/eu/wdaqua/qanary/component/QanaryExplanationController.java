38dc2e153d63a1cb195c0ff7e2c635b73c9660d5
Dennis Schiese <schiesedennis@googlemail.com>
2024-07-24 11:26:16 +0200

    [Done]: Implementation, [WIP]: Tests

--- qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanationController.java
+++ qanary_component-template/src/main/java/eu/wdaqua/qanary/component/QanaryExplanationController.java
@@ -1,5 +1,6 @@
 package eu.wdaqua.qanary.component;
 
+import eu.wdaqua.qanary.exceptions.SparqlQueryFailed;
 import eu.wdaqua.qanary.message.QanaryQuestionAnsweringRun;
 import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 @Controller
 public class QanaryExplanationController {
 
@@ -20,14 +24,14 @@ public class QanaryExplanationController {
     @Autowired
     QanaryExplanation qanaryExplanationService;
 
-    @GetMapping(value = {"/explain/{graphUri}"})
+    @GetMapping(value = {"/explain/{graphUri}/{questionId}"})
     @Operation(
             summary = "Explanation endpoint for Qanary component.", //
             operationId = "explainComponent", //
             description = "Returns an explanation for this component within the specified graph." //
     )
-    public ResponseEntity<?> explainComponent(@RequestBody QanaryQuestionAnsweringRun qanaryQuestionAnsweringRun) { // To be done
-        return new ResponseEntity<>("Not yet implemented", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> explainComponent(@PathVariable String graphUri, @PathVariable String questionId) throws URISyntaxException, IOException, SparqlQueryFailed { // To be done
+        return qanaryExplanationService.explain(graphUri, questionId);
     }
 
 }
