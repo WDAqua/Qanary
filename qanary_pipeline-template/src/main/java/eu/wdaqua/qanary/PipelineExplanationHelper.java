0a69fca7c3ddf765903eb565e0664c37f822dea6
Dennis Schiese <schiesedennis@googlemail.com>
2024-08-19 10:07:04 +0200

    Added documentation for PipelineExplanationHelper

--- qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/PipelineExplanationHelper.java
+++ qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/PipelineExplanationHelper.java
@@ -22,6 +22,11 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Helper class for the creation of pipeline-explanations
+ * :: In this approach, pipelines are being explained by collecting the sub-components explanations. To realize this step of fetching these explanations this
+ * helper class implements several methods to do so.
+ */
 @Component
 @Lazy
 public class PipelineExplanationHelper {
@@ -52,19 +57,34 @@ public class PipelineExplanationHelper {
         this.webClient = WebClient.builder().build();
     }
 
-    public void setExplanationService(String service) {
-        this.explanationService = service;
-    }
-
+    /**
+     * Requests the explanations of the passed components withing the QanaryExplanationData-List
+     * @param data List with objects that contain the component, its corresponding URI and the graph
+     * @return Stream of Strings (= Explanations)
+     */
     public Flux<String> fetchSubComponentExplanations(List<QanaryExplanationData> data) {
         return Flux.fromIterable(data).flatMapSequential(this::getComponentExplanation);
     }
 
+    /**
+     * Requests the explanation for a single components embedded in a {@link eu.wdaqua.qanary.explainability.QanaryExplanationData}
+     * @param data
+     * @return Explanation as Mono
+     */
     public Mono<String> getComponentExplanation(QanaryExplanationData data) {
         return webClient.post().uri(data.getServerHost() + "/explain").bodyValue(data).retrieve().bodyToMono(String.class);
     }
 
-    public String getGraphFromQuestionId(String questionId) throws IOException, URISyntaxException, SparqlQueryFailed {
+    /**
+     * Queries for the sub-graph of the PaC by using the questionId and the pre-defined Qanary components (defined in the corresponding application.properties)
+     * Note: The questionId is the same for all graphs withing one QA process.
+     * Note: The Qanary components are pre-defined for any pipeline-as-component
+     * @param questionId QuestionID of the QA process
+     * @return graph
+     * @throws IOException
+     * @throws SparqlQueryFailed
+     */
+    public String getGraphFromQuestionId(String questionId) throws IOException, SparqlQueryFailed {
         String componentTriples = createComponentTriples();
         String query = QanaryTripleStoreConnector.readFileFromResources(GRAPH_QUERY)
                 .replace("?componentAnnotations", componentTriples)
@@ -73,6 +93,11 @@ public class PipelineExplanationHelper {
         return qanaryTripleStoreConnector.select(query).next().get("g").toString(); // return first
     }
 
+    /**
+     * Creates the triples that represent the used components
+     * Note: Each component must have at least one annotation (if no new data was computed, logs were inserted to the graph)
+     * @return String representing the used components as triples
+     */ // TODO: Different, more robust approach, see Jena Model
     public String createComponentTriples() {
         String triples = "";
         for (String component : QANARY_COMPONENTS) {
@@ -83,10 +108,24 @@ public class PipelineExplanationHelper {
         return triples;
     }
 
+    /**
+     * Requests the explanation for the pipeline with the prior computed sub-component-explanations
+     * @param data Containing the sub-components' explanations
+     * @return explanation for pipeline
+     */
     public String requestPipelineExplanation(QanaryExplanationData data) {
         return webClient.post().uri(this.explanationService).bodyValue(data).retrieve().bodyToMono(String.class).block();
     }
 
+    /**
+     * Requests used components for the case pipeline-as-pipeline
+     * Note: In this case, the pipeline don't define its components statically as they are selected by the user
+     * @param graph graph
+     * @return List of used components
+     * @throws IOException
+     * @throws URISyntaxException
+     * @throws SparqlQueryFailed
+     */
     public List<String> getUsedComponents(String graph) throws IOException, URISyntaxException, SparqlQueryFailed {
         QuerySolutionMap qsm = new QuerySolutionMap();
         qsm.add("graph", ResourceFactory.createResource(graph));
